a7257bf50a1680c96f3a698a66adb1f806a5375d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 23:41:30 +0200

    Decompile GTE vector transform wrapper

--- src/main/PAL/main/func_800690E0.c
+++ src/main/PAL/main/func_800690E0.c
@@ -1,5 +1,22 @@
 #include "common.h"
 
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800690E0", func_800690E0);
+s32 func_800690E0(s32 *arg0, s32 *arg1, s32 arg2) {
+    register s32 xy asm("$8");
+    register s32 z asm("$9");
 
+    xy = arg0[0];
+    z = arg0[1];
+    asm volatile(
+        "mtc2 $8,$0\n"
+        "mtc2 $9,$1\n"
+        "nop\n"
+        "cop2 0x486012\n"
+        "swc2 $9,0($5)\n"
+        "swc2 $10,4($5)\n"
+        "swc2 $11,8($5)"
+        :
+        : "r"(xy), "r"(z), "r"(arg1)
+        : "memory");
+    asm volatile("move $2,$6");
+}
