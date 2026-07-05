acc61961ac9f05875285ee6c1c8b3f88f3bc2fd6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 22:03:14 +0200

    Decompile CD load reset helpers

--- src/main/PAL/main/func_80017BE4.c
+++ src/main/PAL/main/func_80017BE4.c
@@ -1,5 +1,30 @@
 #include "common.h"
 
+extern s32 D_8007BED8;
+extern s16 D_8007C700;
+extern s32 D_8007C704;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80017BE4", func_80017BE4);
+void func_80027634(void);
+s32 func_8006A534(s32 arg0, s32 arg1);
+s32 func_8006A5A4(s32 arg0, void *arg1, s32 arg2);
 
+void func_80017BE4(void) {
+    if (D_8007C700 == 4) {
+        func_80027634();
+    }
+
+    D_8007C700 = 0;
+    D_8007BED8 = 0;
+    D_8007C704 = 0;
+}
+
+s32 func_80017C2C(void) {
+    u8 value;
+
+    if (func_8006A534(1, 0) == 0) {
+        return 0;
+    }
+
+    value = 7;
+    return func_8006A5A4(0xE, &value, 0);
+}
