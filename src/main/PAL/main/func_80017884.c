6fce65e206f528026945c1f1f9715c094919a47c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 01:39:11 +0200

    Decompile asset table helpers

--- src/main/PAL/main/func_80017884.c
+++ src/main/PAL/main/func_80017884.c
@@ -1,7 +1,9 @@
 #include "common.h"
 
 extern u32 D_8007C484;
+extern void *D_8009E698;
 extern void *D_801E424C[];
+extern void *D_801E8A54[];
 
 void LoadImage(void *rect, void *data) asm("func_80065B24");
 
@@ -15,4 +17,13 @@ void func_80017B5C(s32 index) {
     LoadImage(&D_8007C484, D_801E424C[index]);
 }
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80017884", func_80017B94);
+void func_80017B94(void *asset, s32 index) {
+    D_801E8A54[index] = asset;
+}
+
+void func_80017BAC(s32 index) {
+    D_8009E698 = D_801E8A54[index];
+}
+
+asm(".globl func_80017BBC\nfunc_80017BBC = func_80017BAC + 0x10");
+asm(".word 0x03E00008\n.word 0x00000000");
