a43a6651ecd6388d98c5a89b875523f63a339ad3
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:09:43 +0200

    Group early rodata asm unit

--- src/main/PAL/main/func_800180CC.c
+++ src/main/PAL/main/func_800180CC.c
@@ -3,8 +3,4 @@
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_800180CC", func_800180CC_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800180CC", func_800180CC);
-
-#if 0
-void func_800180CC(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800180CC", func_800182D0);
--- src/main/PAL/main/func_800182D0.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800182D0", func_800182D0);
-
-#if 0
-void func_800182D0(void) {
-}
-#endif
