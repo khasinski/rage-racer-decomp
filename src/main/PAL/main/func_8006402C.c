2bb054a56425bb0d928c99cb53dd2b0997c26000
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 00:53:57 +0200

    Decompile MDEC wrapper helpers

--- src/main/PAL/main/func_8006402C.c
+++ src/main/PAL/main/func_8006402C.c
@@ -1,5 +1,28 @@
 #include "common.h"
 
+void func_80064264(void);
+void func_800642F4(void);
+void func_8006438C(void);
+void func_8006DF94(s32 arg0, s32 arg1);
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8006402C", func_8006402C);
+void func_8006402C(void) {
+    func_80064264();
+}
 
+void func_8006404C(void) {
+    func_800642F4();
+}
+
+void func_8006406C(void) {
+    func_8006438C();
+}
+
+void func_8006408C(s32 arg0) {
+    func_8006DF94(0, arg0);
+}
+
+asm(".globl func_8006409C\nfunc_8006409C = func_8006408C + 0x10");
+
+void func_800640B0(s32 arg0) {
+    func_8006DF94(1, arg0);
+}
