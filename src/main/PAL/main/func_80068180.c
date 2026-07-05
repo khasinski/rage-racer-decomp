0bc3db3e934f9cbf210d2cff3953db0fbdb30064
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:50:58 +0200

    Group memory utility helpers

--- src/main/PAL/main/func_80068180.c
+++ src/main/PAL/main/func_80068180.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 void MemFill(u8 *dst, u8 value, s32 count) asm("func_80068180");
+u8 *MemCopy(u8 *dst, u8 *src, s32 count) asm("func_800681BC");
 
 void MemFill(u8 *dst, u8 value, s32 count) {
     volatile s32 unused;
@@ -15,4 +16,31 @@ void MemFill(u8 *dst, u8 value, s32 count) {
             dst++;
         } while (i != end);
     }
+}
+
+u32 func_800681AC[4] __attribute__((section(".text"))) = {
+    0x240A00A0,
+    0x01400008,
+    0x24090049,
+    0,
+};
+
+u8 *MemCopy(u8 *dst, u8 *src, s32 count) {
+    u8 *ret;
+
+    if (dst == 0) {
+        return 0;
+    }
+
+    ret = dst;
+    if (count > 0) {
+        do {
+            *dst = *src;
+            src++;
+            count--;
+            dst++;
+        } while (count > 0);
+    }
+
+    return ret;
 }
