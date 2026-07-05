c53d9d58c94bd9409139b5d9dc8826106746d3b0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 01:42:10 +0200

    Decompile small GTE helpers

--- src/main/PAL/main/func_80069C7C.c
+++ src/main/PAL/main/func_80069C7C.c
@@ -1,3 +1,18 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80069C7C", func_80069C7C);
+s32 func_80069C7C(s32 value) {
+    register s32 ret asm("$2");
+
+    asm volatile(
+        "mtc2 $4,$30\n"
+        "nop\n"
+        "nop\n"
+        "mfc2 $2,$31"
+        : "=r"(ret)
+        : "r"(value));
+    return ret;
+}
+
+asm(".globl func_80069C94\n"
+    "func_80069C94:\n"
+    "nop");
--- src/main/PAL/main/func_80069C98.c
+++ src/main/PAL/main/func_80069C98.c
@@ -1,5 +1,17 @@
 #include "common.h"
 
+s32 func_80069C98(s32 arg0, s32 arg1, s32 arg2) {
+    register s32 ret asm("$2");
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80069C98", func_80069C98);
-
+    asm volatile(
+        "mtc2 $4,$12\n"
+        "mtc2 $6,$14\n"
+        "mtc2 $5,$13\n"
+        "nop\n"
+        "nop\n"
+        "cop2 0x1400006\n"
+        "mfc2 $2,$24"
+        : "=r"(ret)
+        : "r"(arg0), "r"(arg1), "r"(arg2));
+    return ret;
+}
