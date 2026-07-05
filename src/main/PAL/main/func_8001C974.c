f48c00623fdc67416438508a6b6d6d511be52822
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 19:43:46 +0200

    Group menu state asm unit

--- src/main/PAL/main/func_8001C974.c
+++ src/main/PAL/main/func_8001C974.c
@@ -2,8 +2,5 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001C974", func_8001C974);
-
-#if 0
-void func_8001C974(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001C974", func_8001CD54);
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001C974", func_8001CFB4);
--- src/main/PAL/main/func_8001CD54.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001CD54", func_8001CD54);
-
-#if 0
-void func_8001CD54(void) {
-}
-#endif
