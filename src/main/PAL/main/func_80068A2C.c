8287200f29da272b4ca767c995e061a87578b24a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 01:59:08 +0200

    Decompile GTE padding entrypoints

--- src/main/PAL/main/func_80068A2C.c
+++ src/main/PAL/main/func_80068A2C.c
@@ -1,6 +1,13 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80068A2C", func_80068A2C);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80068A2C", func_80068A30);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80068A2C", func_80068A34);
+asm(".globl func_80068A2C\n"
+    "func_80068A2C:\n"
+    "nop\n"
+    ".globl func_80068A30\n"
+    "func_80068A30:\n"
+    "nop\n"
+    ".globl func_80068A34\n"
+    "func_80068A34:\n"
+    "nop");
+
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80068A2C", func_80068A38);
