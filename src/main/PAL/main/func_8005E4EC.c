424bc8739deaeeef70d68d9cba137381fd14ef6e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 19:52:52 +0200

    Group VAB slot asm unit

--- src/main/PAL/main/func_8005E4EC.c
+++ src/main/PAL/main/func_8005E4EC.c
@@ -2,8 +2,5 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E4EC);
-
-#if 0
-void func_8005E4EC(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E600);
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E694);
