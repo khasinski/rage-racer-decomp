1e990224db331faad6309a8f033ed51411c9606b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:08:31 +0200

    Group map rodata asm unit

--- src/main/PAL/main/func_80054D10.c
+++ src/main/PAL/main/func_80054D10.c
@@ -3,8 +3,4 @@
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_80054D10", func_80054D10_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80054D10", func_80054D10);
-
-#if 0
-void func_80054D10(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80054D10", func_800551BC);
--- src/main/PAL/main/func_800551BC.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800551BC", func_800551BC);
-
-#if 0
-void func_800551BC(void) {
-}
-#endif
