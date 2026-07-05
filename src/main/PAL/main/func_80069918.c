60ea78d0f8dacbf46817ec5f243ba4e92ee56ce6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:55:38 +0200

    Group GTE matrix vertex setters

--- src/main/PAL/main/func_80069918.c
+++
@@ -1,6 +0,0 @@
-#include "common.h"
-
-void func_80069918(void *arg0) {
-    asm("lwc2 $2,0($4)");
-    asm("lwc2 $3,4($4)");
-}
--- src/main/PAL/main/func_80069928.c
+++
@@ -1,6 +0,0 @@
-#include "common.h"
-
-void func_80069928(void *arg0) {
-    asm("lwc2 $4,0($4)");
-    asm("lwc2 $5,4($4)");
-}
