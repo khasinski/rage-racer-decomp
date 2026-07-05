10fecc7de7a7443a7b8f0c9d6e5e64bb65ee6008
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 18:07:03 +0200

    Group CD string and callback helpers

--- src/main/PAL/main/func_8006A494.c
+++ src/main/PAL/main/func_8006A494.c
@@ -1,5 +1,11 @@
 #include "common.h"
+#include "psyq/cd.h"
+
 extern s32 D_80099048;
+extern char *D_80099060[];
+extern char *D_800990E0[];
+extern u8 D_800136D0[];
+
 void func_8006BAF0(void);
 
 void func_8006A494(void) {
@@ -11,3 +17,21 @@ s32 func_8006A4B4(s32 arg0) {
     D_80099048 = arg0;
     return old;
 }
+
+char *CdComstr(s32 arg0) {
+    arg0 &= 0xFF;
+    if ((u32)arg0 >= 0x1C) {
+        return (char *)D_800136D0;
+    }
+    return D_80099060[arg0];
+}
+
+char *CdIntstr(s32 arg0) {
+    arg0 &= 0xFF;
+    if ((u32)arg0 >= 7) {
+        return (char *)D_800136D0;
+    }
+    return D_800990E0[arg0];
+}
+
+INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8006A534);
--- src/main/PAL/main/func_8006A534.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8006A534);
-
-#if 0
-void func_8006A534(void) {
-}
-#endif
