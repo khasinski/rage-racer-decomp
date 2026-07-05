a43774660a321f8c3faa6607f094390825c78b19
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:04:25 +0200

    Group CD wait rodata asm unit

--- src/main/PAL/main/func_8005ECE0.c
+++ src/main/PAL/main/func_8005ECE0.c
@@ -3,8 +3,4 @@
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005ECE0_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005ECE0);
-
-#if 0
-void func_8005ECE0(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005EF44);
