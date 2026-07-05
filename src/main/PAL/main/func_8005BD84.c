e1b09d39aee46893fb8b826dfe205723ded2edb6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 00:51:09 +0200

    Decompile clamp helper

--- src/main/PAL/main/func_8005BD84.c
+++ src/main/PAL/main/func_8005BD84.c
@@ -1,5 +1,30 @@
 #include "common.h"
 
+void func_80043134(s32 arg0);
+void func_8005E834(s32 arg0);
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005BD84", func_8005BD84);
+void func_8005BD84(s32 arg0) {
+    register s32 value asm("$16");
 
+    value = arg0;
+    asm("" : "=r"(value) : "0"(value));
+    if (value < 0) {
+        goto negative;
+    }
+    if (arg0 < 0x10) {
+        goto call;
+    }
+
+    arg0 = 0xF;
+    goto call;
+
+negative:
+    arg0 = 0;
+
+call:
+    value = arg0;
+    func_80043134(arg0);
+    func_8005E834(value);
+}
+
+asm(".globl func_8005BDB0\nfunc_8005BDB0 = func_8005BD84 + 0x2C");
