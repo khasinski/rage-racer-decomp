f4a3c742f5f09b321914f1fa0c1033ed3c287765
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:01:49 +0200

    Group loaded audio voice asm unit

--- src/main/PAL/main/func_8005D8EC.c
+++ src/main/PAL/main/func_8005D8EC.c
@@ -2,8 +2,4 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D8EC", func_8005D8EC);
-
-#if 0
-void func_8005D8EC(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D8EC", func_8005D9F8);
--- src/main/PAL/main/func_8005D9F8.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D9F8", func_8005D9F8);
-
-#if 0
-void func_8005D9F8(void) {
-}
-#endif
