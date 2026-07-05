8774e92cb20182e9644f2f7d5df0628410f55252
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 01:16:16 +0200

    Decompile syscall helper

--- src/main/PAL/main/func_80063220.c
+++ src/main/PAL/main/func_80063220.c
@@ -1,3 +1,7 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80063220", func_80063220);
+s32 func_80063220(void) {
+    asm volatile(
+        "addiu $4,$0,2\n"
+        "syscall");
+}
