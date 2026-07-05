8826dbb006cb59372b018e351e8f0fbb64200ad6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 23:57:53 +0200

    Decompile transition state init helper

--- src/main/PAL/main/func_80026B88.c
+++ src/main/PAL/main/func_80026B88.c
@@ -1,5 +1,29 @@
 #include "common.h"
 
+extern s32 D_8019C768;
+extern s32 D_801E42E0;
+extern s32 D_801E42A0;
+extern s32 D_801E42E4;
+extern s32 D_801E4178;
+extern s32 D_801E6824;
+extern s32 D_801E40B8;
+extern s32 D_8009E66C;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80026B88", func_80026B88);
+void func_80065860(s32 arg0);
+void func_8001BE9C(s32 arg0, s32 arg1, s32 arg2);
 
+void func_80026B88(void) {
+    func_80065860(0);
+    func_8001BE9C(0, 0, 0);
+
+    D_8019C768 = 0x80;
+    D_801E42E0 = 0x108;
+    D_801E42A0 = -4;
+    D_801E42E4 = 0x20;
+    D_801E4178 = 0;
+    D_801E6824 = 0;
+    D_801E40B8 = 0;
+    D_8009E66C = 3;
+}
+
+asm(".globl func_80026C00\nfunc_80026C00 = func_80026B88 + 0x78");
