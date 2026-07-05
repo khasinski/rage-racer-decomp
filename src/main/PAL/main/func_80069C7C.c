65686527801d08c249b4f197df14ca5b536b3b8b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 18:00:36 +0200

    Group GTE Lzc padding

--- src/main/PAL/main/func_80069C7C.c
+++ src/main/PAL/main/func_80069C7C.c
@@ -8,3 +8,5 @@ s32 Lzc(s32 arg0) {
     asm("nop");
     asm("mfc2 $2,$31");
 }
+
+u32 func_80069C94 __attribute__((section(".text"))) = 0;
--- src/main/PAL/main/func_80069C94.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-u32 func_80069C94 __attribute__((section(".text"))) = 0;
