5f1e631c516b91214a5fbb7668214cac3abf1393
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 22:51:28 +0200

    Decompile VRAM page sync loop

--- src/main/PAL/main/func_8001A030.c
+++ src/main/PAL/main/func_8001A030.c
@@ -1,5 +1,23 @@
 #include "common.h"
 
+extern s32 D_8019CA6C;
+extern s32 D_801E8AF8;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001A030", func_8001A030);
+void func_80019F24(void);
+s32 func_8006DD30(s32 arg0);
 
+void func_8001A030(void) {
+    while (D_8019CA6C != D_801E8AF8) {
+        if (func_8006DD30(1) >= 471) {
+            break;
+        }
+
+        if (D_8019CA6C < D_801E8AF8) {
+            func_80019F24();
+            D_8019CA6C++;
+        } else {
+            D_8019CA6C--;
+            func_80019F24();
+        }
+    }
+}
