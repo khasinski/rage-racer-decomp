14b3080a6981b9fb4e324ffc9da8d0119dd0e2e2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 19:03:14 +0200

    Group libspu reverb work area helper

--- src/main/PAL/lib/libspu/SpuSetReverbModeParam.c
+++ src/main/PAL/lib/libspu/SpuSetReverbModeParam.c
@@ -1,8 +1,14 @@
 #include "psyq/spu.h"
+#include "psyq/kernel.h"
 
 extern s32 D_8009A720;
 extern SpuRevAttrState D_8009A728;
+extern s32 D_8009A768;
+extern s32 D_8009A770[];
 extern SpuRxx *D_8009AB7C;
+extern s32 D_8009AB94;
+extern s32 _spu_mem_mode_unitM asm("D_8009ABA0");
+extern volatile s32 D_8009ABB0;
 extern volatile u16 *D_8009AB7C_half asm("D_8009AB7C");
 extern s32 D_8009ABE0[];
 extern SpuReverbRegAttr D_8009AC30[];
@@ -261,4 +267,64 @@ void _spu_setReverbAttr(SpuReverbRegAttr *attr) {
     if (set_all || (mask & 0x80000000)) {
         D_8009AB7C_half[0xFF] = attr->vRIN;
     }
+}
+
+s32 SpuClearReverbWorkArea(u32 mode) {
+    volatile s32 callback;
+    s32 old_transmode;
+    u32 size;
+    u32 addr;
+    s32 chunk_active;
+    u32 chunk_size;
+    s32 restored_transmode;
+
+    callback = 0;
+    restored_transmode = 0;
+    if ((mode >= 0xA) || (_SpuIsInAllocateArea_(D_8009ABE0[mode]) != 0)) {
+        return -1;
+    }
+
+    if (mode == 0) {
+        size = 0x10 << _spu_mem_mode_unitM;
+        addr = 0xFFF0 << _spu_mem_mode_unitM;
+    } else {
+        size = (0x10000 - D_8009ABE0[mode]) << _spu_mem_mode_unitM;
+        addr = D_8009ABE0[mode] << _spu_mem_mode_unitM;
+    }
+
+    old_transmode = D_8009AB94;
+    if (D_8009AB94 == 1) {
+        D_8009AB94 = 0;
+        restored_transmode = 1;
+    }
+
+    chunk_active = 1;
+    if (D_8009ABB0 != 0) {
+        callback = D_8009ABB0;
+        D_8009ABB0 = 0;
+    }
+
+    do {
+        chunk_size = size;
+        if (size >= 0x401) {
+            chunk_size = 0x400;
+        } else {
+            chunk_active = 0;
+        }
+
+        _spu_t(2, addr);
+        _spu_t(1);
+        _spu_t(3, D_8009A770, chunk_size);
+        WaitEvent(D_8009A768);
+        size -= 0x400;
+        addr += 0x400;
+    } while (chunk_active != 0);
+
+    if (restored_transmode != 0) {
+        D_8009AB94 = old_transmode;
+    }
+    if (callback != 0) {
+        D_8009ABB0 = callback;
+    }
+    return 0;
 }
