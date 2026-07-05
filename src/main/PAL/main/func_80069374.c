608be0a90080997b565fc4e056c9828230104874
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:54:40 +0200

    Group GTE matrix readback helpers

--- src/main/PAL/main/func_80069374.c
+++ src/main/PAL/main/func_80069374.c
@@ -18,3 +18,47 @@ void func_80069374(void *arg0) {
     asm("sw $9,24($4)");
     asm("sw $10,28($4)");
 }
+
+void func_800693BC(void *arg0) {
+    asm("cfc2 $8,$8");
+    asm("cfc2 $9,$9");
+    asm("cfc2 $10,$10");
+    asm("cfc2 $11,$11");
+    asm("cfc2 $12,$12");
+    asm("sw $8,0($4)");
+    asm("sw $9,4($4)");
+    asm("sw $10,8($4)");
+    asm("sw $11,12($4)");
+    asm("sw $12,16($4)");
+    asm("cfc2 $8,$13");
+    asm("cfc2 $9,$14");
+    asm("cfc2 $10,$15");
+    asm("sw $8,20($4)");
+    asm("sw $9,24($4)");
+    asm("sw $10,28($4)");
+}
+
+void func_80069404(void *arg0) {
+    asm("cfc2 $8,$16");
+    asm("cfc2 $9,$17");
+    asm("cfc2 $10,$18");
+    asm("cfc2 $11,$19");
+    asm("cfc2 $12,$20");
+    asm("sw $8,0($4)");
+    asm("sw $9,4($4)");
+    asm("sw $10,8($4)");
+    asm("sw $11,12($4)");
+    asm("sw $12,16($4)");
+    asm("cfc2 $8,$21");
+    asm("cfc2 $9,$22");
+    asm("cfc2 $10,$23");
+    asm("sw $8,20($4)");
+    asm("sw $9,24($4)");
+    asm("sw $10,28($4)");
+}
+
+u32 func_8006944C[3] __attribute__((section(".text"))) = {
+    0,
+    0,
+    0,
+};
--- src/main/PAL/main/func_8006944C.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-u32 func_8006944C[3] __attribute__((section(".text"))) = {
-    0,
-    0,
-    0,
-};
