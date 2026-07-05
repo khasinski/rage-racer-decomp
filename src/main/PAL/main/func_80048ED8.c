e6ffa914865172a325f5e498979bba69885745cf
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:13:35 +0200

    Group effect tail rodata asm unit

--- src/main/PAL/main/func_80048ED8.c
+++ src/main/PAL/main/func_80048ED8.c
@@ -3,8 +3,4 @@
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_80048ED8", func_80048ED8_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80048ED8", func_80048ED8);
-
-#if 0
-void func_80048ED8(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80048ED8", func_80049418);
--- src/main/PAL/main/func_80049418.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80049418", func_80049418);
-
-#if 0
-void func_80049418(void) {
-}
-#endif
