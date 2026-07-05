c95e88d6054df991141514f813873149be16de56
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:39:19 +0200

    Group GPU environment dump helpers

--- src/main/PAL/main/func_80065234.c
+++ src/main/PAL/main/func_80065234.c
@@ -8,6 +8,10 @@ extern char D_800133B4[];
 extern char D_800133C4[];
 extern char D_800133DC[];
 extern char D_800133E8[];
+extern char D_800133F4[];
+extern char D_80013410[];
+extern char D_8001342C[];
+extern char D_80013438[];
 extern void (*D_800941E4)(char *, ...);
 
 typedef struct {
@@ -26,7 +30,21 @@ typedef struct {
     u8 b1;
 } UnkFunc80065234;
 
+typedef struct {
+    s16 x0;
+    s16 y0;
+    s16 x1;
+    s16 y1;
+    s16 x2;
+    s16 y2;
+    s16 x3;
+    s16 y3;
+    u8 b0;
+    u8 b1;
+} UnkFunc800653B4;
+
 void DumpDrawEnv(UnkFunc80065234 *arg0) asm("func_80065234");
+void DumpDispEnv(UnkFunc800653B4 *arg0) asm("func_800653B4");
 
 void DumpDrawEnv(UnkFunc80065234 *arg0) {
     s32 mode;
@@ -56,4 +74,11 @@ high_mode:
 low_mode:
     value = arg0->mode;
     D_800941E4(D_80013374, (value >> 7) & 3, (value >> 5) & 3, (value << 6) & 0x7C0, ((value << 4) & 0x100) + ((value >> 2) & 0x200));
+}
+
+void DumpDispEnv(UnkFunc800653B4 *arg0) {
+    D_800941E4(D_800133F4, arg0->x0, arg0->y0, arg0->x1, arg0->y1);
+    D_800941E4(D_80013410, arg0->x2, arg0->y2, arg0->x3, arg0->y3);
+    D_800941E4(D_8001342C, arg0->b0);
+    D_800941E4(D_80013438, arg0->b1);
 }
