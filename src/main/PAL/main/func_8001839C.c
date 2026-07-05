38708e968249aa55a533fd5719532fe8e4d0be27
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 22:10:58 +0200

    Decompile asset header load handlers

--- src/main/PAL/main/func_8001839C.c
+++ src/main/PAL/main/func_8001839C.c
@@ -1,6 +1,85 @@
 #include "common.h"
 
+typedef struct AssetTripleHeader {
+    s32 firstOffset;
+    s32 secondOffset;
+    s32 thirdOffset;
+} AssetTripleHeader;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001839C", func_8001839C);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001839C", func_80018410);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001839C", func_80018484);
+extern s32 D_8007BED8;
+extern s32 D_8007C704;
+extern AssetTripleHeader *D_8019C904;
+extern void *D_8019C754;
+extern void *D_801E8AB0;
+extern void *D_801F17A8;
+
+s32 func_80017C78(s32 assetIndex, void *dst);
+void func_80042C94(void);
+void func_8005B9CC(void);
+
+s32 func_8001839C(void) {
+    register s32 loadType asm("$16");
+
+    if (D_8007BED8 != 0) {
+        return 1;
+    }
+
+    loadType = 3;
+    if (D_8007C704 == loadType) {
+        D_8007C704 = 0;
+        return 0;
+    }
+
+    func_80042C94();
+    D_8007C704 = loadType;
+    D_8007BED8 = 2;
+    return 1;
+}
+
+s32 func_80018410(void) {
+    register s32 loadType asm("$16");
+
+    if (D_8007BED8 != 0) {
+        return 1;
+    }
+
+    loadType = 3;
+    if (D_8007C704 == loadType) {
+        D_8007C704 = 0;
+        return 0;
+    }
+
+    func_80042C94();
+    D_8007C704 = loadType;
+    D_8007BED8 = 1;
+    return 1;
+}
+
+void func_80018484(void) {
+    register AssetTripleHeader *header asm("$2");
+    register s32 firstOffset asm("$3");
+    register s32 secondOffset asm("$3");
+    register s32 thirdOffset asm("$4");
+
+    switch (D_8007BED8) {
+    case 1:
+        func_8005B9CC();
+        D_8007BED8 = 2;
+    case 2:
+        if (func_80017C78(7, D_8019C904) != 0) {
+            header = D_8019C904;
+            firstOffset = *(volatile s32 *)&header->firstOffset;
+            thirdOffset = *(volatile s32 *)&header->thirdOffset;
+            D_801F17A8 = (void *)((u8 *)header + firstOffset);
+            secondOffset = *(volatile s32 *)&header->secondOffset;
+            __asm__ volatile("" ::: "memory");
+            D_8007BED8 = 0;
+            secondOffset = (s32)((u8 *)header + secondOffset);
+            header = (AssetTripleHeader *)((u8 *)header + thirdOffset);
+            __asm__ volatile("" ::: "memory");
+            D_8019C754 = (void *)secondOffset;
+            D_801E8AB0 = header;
+        }
+        break;
+    }
+}
