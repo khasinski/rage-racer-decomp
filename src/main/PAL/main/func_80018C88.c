18a5e9e072be578a92c6573199a85154634b808d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 22:38:24 +0200

    Decompile random course asset selector

--- src/main/PAL/main/func_80018C88.c
+++ src/main/PAL/main/func_80018C88.c
@@ -1,5 +1,37 @@
 #include "common.h"
 
+extern s32 D_8007BED8;
+extern s32 D_8007C704;
+extern s32 D_8009E6A4;
+extern s16 D_8019CABC;
+extern s32 D_801E428C;
+extern s16 D_801E4DAC;
+extern s32 D_801E772C[];
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80018C88", func_80018C88);
+s32 func_800632B0(void);
+void func_80017BE4(void);
 
+s32 func_80018C88(void) {
+    s32 value;
+
+    if (D_8007BED8 != 0) {
+        func_80017BE4();
+    }
+
+    if (D_801E4DAC == 0) {
+        value = (func_800632B0() & 0xFFF) % (D_801E772C[D_8019CABC] + 1);
+        D_8009E6A4 = value;
+        if (((D_801E428C & 3) == 3) && (value < 2)) {
+            D_8009E6A4 = ((func_800632B0() & 0xFFF) % (D_801E772C[D_8019CABC] - 1)) + 2;
+        }
+    }
+
+    if (D_8007C704 == 8) {
+        D_8007C704 = 0;
+        return 0;
+    }
+
+    D_8007C704 = 8;
+    D_8007BED8 = 1;
+    return 1;
+}
