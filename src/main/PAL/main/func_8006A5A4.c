63e97cf5c8c479a77cab25ae7fb79abf3f2f3f13
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 18:07:46 +0200

    Group CD command wrappers

--- src/main/PAL/main/func_8006A5A4.c
+++ src/main/PAL/main/func_8006A5A4.c
@@ -3,7 +3,4 @@
 
 INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8006A5A4);
 
-#if 0
-void func_8006A5A4(void) {
-}
-#endif
+INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8006A6DC);
--- src/main/PAL/main/func_8006A6DC.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8006A6DC);
-
-#if 0
-void func_8006A6DC(void) {
-}
-#endif
