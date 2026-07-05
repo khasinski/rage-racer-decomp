93dd81233520b21669e55e6535482b8503109ca2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 19:02:22 +0200

    Group libspu reverb parameter helpers

--- src/main/PAL/lib/libspu/SpuSetReverbModeParam.c
+++ src/main/PAL/lib/libspu/SpuSetReverbModeParam.c
@@ -3,6 +3,7 @@
 extern s32 D_8009A720;
 extern SpuRevAttrState D_8009A728;
 extern SpuRxx *D_8009AB7C;
+extern volatile u16 *D_8009AB7C_half asm("D_8009AB7C");
 extern s32 D_8009ABE0[];
 extern SpuReverbRegAttr D_8009AC30[];
 
@@ -158,4 +159,106 @@ s32 SpuSetReverbModeParam(SpuReverbAttr *attr) {
         }
     }
     return 0;
+}
+
+void _spu_setReverbAttr(SpuReverbRegAttr *attr) {
+    u32 mask = attr->flags;
+    s32 set_all = attr->flags == 0;
+
+    if (set_all || (mask & 0x1)) {
+        D_8009AB7C_half[0xE0] = attr->dAPF1;
+    }
+    if (set_all || (mask & 0x2)) {
+        D_8009AB7C_half[0xE1] = attr->dAPF2;
+    }
+    if (set_all || (mask & 0x4)) {
+        D_8009AB7C_half[0xE2] = attr->vIIR;
+    }
+    if (set_all || (mask & 0x8)) {
+        D_8009AB7C_half[0xE3] = attr->vCOMB1;
+    }
+    if (set_all || (mask & 0x10)) {
+        D_8009AB7C_half[0xE4] = attr->vCOMB2;
+    }
+    if (set_all || (mask & 0x20)) {
+        D_8009AB7C_half[0xE5] = attr->vCOMB3;
+    }
+    if (set_all || (mask & 0x40)) {
+        D_8009AB7C_half[0xE6] = attr->vCOMB4;
+    }
+    if (set_all || (mask & 0x80)) {
+        D_8009AB7C_half[0xE7] = attr->vWALL;
+    }
+    if (set_all || (mask & 0x100)) {
+        D_8009AB7C_half[0xE8] = attr->vAPF1;
+    }
+    if (set_all || (mask & 0x200)) {
+        D_8009AB7C_half[0xE9] = attr->vAPF2;
+    }
+    if (set_all || (mask & 0x400)) {
+        D_8009AB7C_half[0xEA] = attr->mLSAME;
+    }
+    if (set_all || (mask & 0x800)) {
+        D_8009AB7C_half[0xEB] = attr->mRSAME;
+    }
+    if (set_all || (mask & 0x1000)) {
+        D_8009AB7C_half[0xEC] = attr->mLCOMB1;
+    }
+    if (set_all || (mask & 0x2000)) {
+        D_8009AB7C_half[0xED] = attr->mRCOMB1;
+    }
+    if (set_all || (mask & 0x4000)) {
+        D_8009AB7C_half[0xEE] = attr->mLCOMB2;
+    }
+    if (set_all || (mask & 0x8000)) {
+        D_8009AB7C_half[0xEF] = attr->mRCOMB2;
+    }
+    if (set_all || (mask & 0x10000)) {
+        D_8009AB7C_half[0xF0] = attr->dLSAME;
+    }
+    if (set_all || (mask & 0x20000)) {
+        D_8009AB7C_half[0xF1] = attr->dRSAME;
+    }
+    if (set_all || (mask & 0x40000)) {
+        D_8009AB7C_half[0xF2] = attr->mLDIFF;
+    }
+    if (set_all || (mask & 0x80000)) {
+        D_8009AB7C_half[0xF3] = attr->mRDIFF;
+    }
+    if (set_all || (mask & 0x100000)) {
+        D_8009AB7C_half[0xF4] = attr->mLCOMB3;
+    }
+    if (set_all || (mask & 0x200000)) {
+        D_8009AB7C_half[0xF5] = attr->mRCOMB3;
+    }
+    if (set_all || (mask & 0x400000)) {
+        D_8009AB7C_half[0xF6] = attr->mLCOMB4;
+    }
+    if (set_all || (mask & 0x800000)) {
+        D_8009AB7C_half[0xF7] = attr->mRCOMB4;
+    }
+    if (set_all || (mask & 0x1000000)) {
+        D_8009AB7C_half[0xF8] = attr->dLDIFF;
+    }
+    if (set_all || (mask & 0x2000000)) {
+        D_8009AB7C_half[0xF9] = attr->dRDIFF;
+    }
+    if (set_all || (mask & 0x4000000)) {
+        D_8009AB7C_half[0xFA] = attr->mLAPF1;
+    }
+    if (set_all || (mask & 0x8000000)) {
+        D_8009AB7C_half[0xFB] = attr->mRAPF1;
+    }
+    if (set_all || (mask & 0x10000000)) {
+        D_8009AB7C_half[0xFC] = attr->mLAPF2;
+    }
+    if (set_all || (mask & 0x20000000)) {
+        D_8009AB7C_half[0xFD] = attr->mRAPF2;
+    }
+    if (set_all || (mask & 0x40000000)) {
+        D_8009AB7C_half[0xFE] = attr->vLIN;
+    }
+    if (set_all || (mask & 0x80000000)) {
+        D_8009AB7C_half[0xFF] = attr->vRIN;
+    }
 }
