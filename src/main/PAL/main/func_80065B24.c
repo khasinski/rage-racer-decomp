a3a256c3b93cc50b03ae14af373f2dbdd2358fe0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:42:30 +0200

    Group GPU image transfer helpers

--- src/main/PAL/main/func_80065B24.c
+++ src/main/PAL/main/func_80065B24.c
@@ -1,20 +1,88 @@
 #include "common.h"
 
-typedef struct GpuFuncs {
+typedef struct LoadImageGpuFuncs {
     u8 pad0[0x8];
     void (*send)(u32 cmd, void *rect, s32 size, u32 data);
     u8 padC[0x20 - 0xC];
     u32 loadImage;
-} GpuFuncs;
+} LoadImageGpuFuncs;
 
-extern GpuFuncs *g_GpuFuncs asm("D_800941E0");
+typedef struct StoreImageGpuFuncs {
+    u8 pad0[0x8];
+    void (*send)(u32 cmd, void *rect, s32 size, u32 data);
+    u8 padC[0x1C - 0xC];
+    u32 storeImage;
+} StoreImageGpuFuncs;
+
+typedef struct MoveImageGpuFuncs {
+    u8 pad0[0x8];
+    s32 (*send)(u32 cmd, void *buf, s32 size, u32 data);
+    u8 padC[0x18 - 0xC];
+    u32 moveImage;
+} MoveImageGpuFuncs;
+
+typedef struct RectLike {
+    u32 xy;
+    s16 w;
+    s16 h;
+} RectLike;
+
+extern LoadImageGpuFuncs *g_GpuFuncs asm("D_800941E0");
+extern StoreImageGpuFuncs *g_GpuFuncsStore asm("D_800941E0");
+extern MoveImageGpuFuncs *g_GpuFuncsMove asm("D_800941E0");
+extern u32 D_80094298;
+extern u32 D_8009429C;
+extern u32 D_800942A0;
 
 void CheckPrim(char *arg0, void *arg1) asm("func_80065968");
 extern char D_80013578[];
+extern char D_80013584[];
+extern char D_80013590[];
 
 void LoadImage(void *arg0, void *arg1) asm("func_80065B24");
+void StoreImage(void *arg0, void *arg1) asm("func_80065B88");
+s32 MoveImage(RectLike *arg0, u32 arg1, u32 arg2) asm("func_80065BEC");
 
 void LoadImage(void *arg0, void *arg1) {
     CheckPrim(D_80013578, arg0);
     g_GpuFuncs->send(g_GpuFuncs->loadImage, arg0, 8, (u32)arg1);
+}
+
+void StoreImage(void *arg0, void *arg1) {
+    CheckPrim(D_80013584, arg0);
+    g_GpuFuncsStore->send(g_GpuFuncsStore->storeImage, arg0, 8, (u32)arg1);
+}
+
+s32 MoveImage(RectLike *arg0, u32 arg1, u32 arg2) {
+    CheckPrim(D_80013590, arg0);
+    if (arg0->w == 0) {
+        return -1;
+    }
+    if (arg0->h != 0) {
+        register u32 *buf asm("$5");
+        register MoveImageGpuFuncs *gpu asm("$3");
+        register u32 xy asm("$4");
+        register s32 size asm("$6");
+        register s32 data asm("$7");
+        register u32 packed asm("$2");
+        register u32 low asm("$3");
+        u32 wh;
+
+        packed = arg2 << 0x10;
+        low = arg1 & 0xFFFF;
+        packed |= low;
+        asm("" : "=r"(packed) : "0"(packed));
+        buf = &D_80094298;
+        xy = arg0->xy;
+        gpu = g_GpuFuncsMove;
+        size = 0x14;
+        D_8009429C = packed;
+        *buf = xy;
+        wh = *(u32 *)&arg0->w;
+        data = 0;
+        D_800942A0 = wh;
+        asm("" : : "r"(wh) : "memory");
+        return gpu->send(gpu->moveImage, buf - 2, size, data);
+    }
+    return -1;
 }
