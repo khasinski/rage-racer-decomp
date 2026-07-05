831d046260dbf10370294582ce73160e1da13c68
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:53:03 +0200

    Group sqrt normalization helpers

--- src/main/PAL/main/func_80068738.c
+++ src/main/PAL/main/func_80068738.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+s32 Lzc(s32 arg0) asm("func_80069C7C");
+
 s32 func_80068738(s32 arg0) {
     s32 data[16];
     register s32 *hi asm("$8");
@@ -80,4 +82,35 @@ s32 func_80068738(s32 arg0) {
     } while (i < 7);
 
     return data[7];
+}
+
+s32 func_8006888C(s32 arg0) {
+    s32 bits;
+    s32 shift;
+    s32 value;
+    s32 ret;
+
+    if (arg0 == 0) {
+        return 0;
+    }
+
+    bits = 8 - Lzc(arg0);
+    if (bits >= 0) {
+        shift = bits >> 1;
+        value = arg0 >> (shift * 2);
+    } else {
+        shift = (bits >> 1) + 1;
+        value = arg0 << -(((bits >> 1) + 1) * 2);
+    }
+
+    shift -= 6;
+    if (shift < 0) {
+        ret = func_80068738(value) >> -shift;
+        goto done;
+    }
+
+    ret = func_80068738(value) << shift;
+
+done:
+    return ret;
 }
