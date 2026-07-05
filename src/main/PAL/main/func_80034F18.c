44896626cc2c5fe30d0cb7c8d4149dc2c994445d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 23:45:08 +0200

    Decompile frame scale helper

--- src/main/PAL/main/func_80034F18.c
+++ src/main/PAL/main/func_80034F18.c
@@ -1,3 +1,14 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80034F18", func_80034F18);
+s32 func_80034F18(s32 arg0, s32 arg1) {
+    register s32 quotient asm("$3");
+    s32 remainder;
+    register s32 remainderPart asm("$4");
+    s32 quotientPart;
+
+    quotient = arg0 / 25;
+    remainder = arg0 - (quotient * 25);
+    remainderPart = (remainder * 40) + arg1;
+    quotientPart = quotient * 1000;
+    return quotientPart + remainderPart;
+}
