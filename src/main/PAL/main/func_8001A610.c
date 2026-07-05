a481594edfc8f34a812bee6c4e74eb77e3761d55
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 22:53:05 +0200

    Decompile scratchpad rotation setup

--- src/main/PAL/main/func_8001A610.c
+++ src/main/PAL/main/func_8001A610.c
@@ -1,5 +1,25 @@
 #include "common.h"
+#include "psyq/gte.h"
 
+extern Matrix D_8019CB18;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8001A610", func_8001A610);
+void func_8001A4C0(Matrix *mtx, s32 angle);
+void func_8001A530(Matrix *mtx, s32 angle);
+void func_8001A5A0(Matrix *mtx, s32 angle);
+void func_80068B98(Matrix *lhs, Matrix *rhs, Matrix *out);
+void func_80069568(Matrix *lhs, Matrix *rhs);
+void func_80069858(Matrix *mtx);
 
+void func_8001A610(void) {
+    Matrix mtx;
+    Matrix *scratch = (Matrix *)0x1F800028;
+
+    func_8001A530(scratch, *(s32 *)0x1F80001C);
+    func_8001A5A0(&mtx, *(s32 *)0x1F800018);
+    func_80069568(&mtx, scratch);
+    func_8001A4C0(&mtx, *(s32 *)0x1F800020);
+    func_80069568(&mtx, scratch);
+    func_8001A530(&mtx, 0x800);
+    func_80068B98(&mtx, scratch, &D_8019CB18);
+    func_80069858(scratch);
+}
