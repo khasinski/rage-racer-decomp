0f8581a6db1e1ea720f844fbd066841ea55a2266
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:03:16 +0200

    Group audio cue internal asm unit

--- src/main/PAL/main/func_8005C914.c
+++ src/main/PAL/main/func_8005C914.c
@@ -2,8 +2,5 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005C914", func_8005C914);
-
-#if 0
-void func_8005C914(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005C914", func_8005CDB0);
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005C914", func_8005D050);
--- src/main/PAL/main/func_8005D050.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D050", func_8005D050);
-
-#if 0
-void func_8005D050(void) {
-}
-#endif
