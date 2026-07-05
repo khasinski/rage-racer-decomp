6bec286046299d3ebffba21ee12ddff7879fe0a1
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 19:51:28 +0200

    Group audio parameter asm unit

--- src/main/PAL/main/func_8005B070.c
+++ src/main/PAL/main/func_8005B070.c
@@ -2,8 +2,6 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B070);
-
-#if 0
-void func_8005B070(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B190);
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B204);
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B2F0);
--- src/main/PAL/main/func_8005B190.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B190", func_8005B190);
-
-#if 0
-void func_8005B190(void) {
-}
-#endif
