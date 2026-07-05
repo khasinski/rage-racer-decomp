f797f8b32bcdf27c9b8aa8a388598ad0f5f17e4b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 21:45:50 +0200

    Name libc memmove helper

--- src/main/PAL/main/LibcMemmove.c
+++ src/main/PAL/main/LibcMemmove.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
-void *func_80063B9C(u8 *dest, u8 *src, s32 count) {
+void *LibcMemmove(u8 *dest, u8 *src, s32 count) asm("func_80063B9C");
+void *LibcMemmove(u8 *dest, u8 *src, s32 count) {
     if (dest >= src) {
         while (count-- > 0) {
             dest[count] = src[count];
