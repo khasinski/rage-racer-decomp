aa0fa3f4eed0fa9805ee51e12943257e86fa1edd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 18:06:22 +0200

    Group CD TOC initialization helpers

--- src/main/PAL/main/func_8006A0AC.c
+++ src/main/PAL/main/func_8006A0AC.c
@@ -4,9 +4,17 @@ extern s32 D_80099048;
 extern u8 D_80013678[];
 extern u8 D_80013688[];
 extern u8 D_800136A4[];
+extern u8 D_800136B8[];
 
+s32 func_8006A428(s32 arg0);
 s32 func_8006A574(s32 arg0);
+s32 func_8006A58C(s32 arg0);
 s32 func_8006A808(s32 arg0, u8 *arg1, u8 *arg2);
+void func_8006A360(void);
+void func_8006A388(void);
+void func_8006A3B0(void);
+void func_8006A3D8(u32 arg0, s32 arg1);
+void func_8002785C(void *arg0);
 void func_8001674C(u8 *arg0, ...);
 
 s32 func_8006A0AC(s32 arg0, u8 *arg1) {
@@ -117,4 +125,37 @@ fail:
     }
     func_8006A574(oldHandler);
     return 0;
+}
+
+s32 func_8006A2D0(void) {
+    s32 retries;
+
+    retries = 4;
+loop:
+    if (func_8006A428(1) != 1) {
+        retries--;
+        if (retries != -1) {
+            goto loop;
+        }
+
+        func_8001674C(D_800136B8);
+        return 0;
+    }
+
+    func_8006A574((s32)func_8006A360);
+    func_8006A58C((s32)func_8006A388);
+    func_8002785C(func_8006A3B0);
+    return 1;
+}
+
+void func_8006A360(void) {
+    func_8006A3D8(0xF0000003, 0x20);
+}
+
+void func_8006A388(void) {
+    func_8006A3D8(0xF0000003, 0x40);
+}
+
+void func_8006A3B0(void) {
+    func_8006A3D8(0xF0000003, 0x40);
 }
