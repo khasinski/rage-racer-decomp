795a0517fb6b5f56c96e9dd744584fa34a539503
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 20:00:11 +0200

    Group sound cue helper asm unit

--- src/main/PAL/main/func_8005D414.c
+++ src/main/PAL/main/func_8005D414.c
@@ -2,8 +2,4 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D414", func_8005D414);
-
-#if 0
-void func_8005D414(void) {
-}
-#endif
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005D414", func_8005D530);
