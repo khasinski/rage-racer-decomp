c9b4c923814cf2978b57678f1cbcd3aafed21ad0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 17:38:12 +0200

    Group GPU status helpers

--- src/main/PAL/main/func_800640D4.c
+++ src/main/PAL/main/func_800640D4.c
@@ -10,12 +10,18 @@ extern volatile u32 *D_80083194;
 extern volatile u32 *D_80083198;
 extern volatile u32 *D_8008319C;
 extern u8 D_800132C8[];
+extern u8 D_800132E4[];
+extern u8 D_800132F4[];
+extern u8 D_80013304[];
+extern u8 D_8001332C[];
+extern u8 D_80013364[];
 extern u8 D_8008305C[];
 extern u8 D_800830E0[];
 
 s32 func_800642F4(void);
 s32 func_8006438C(void);
-void func_8001674C(u8 *arg0);
+s32 func_80064424(u8 *arg0);
+void func_8001674C(u8 *arg0, ...);
 
 void func_800641D0(volatile u32 *arg0, s32 arg1);
 
@@ -69,4 +75,70 @@ void func_80064264(volatile u32 *arg0, s32 arg1) {
     *D_80083170 = (u32)arg0;
     *D_80083174 = ((u32)arg1 >> 5 << 16) | 0x20;
     *D_80083178 = 0x01000200;
+}
+
+s32 func_800642F4(void) {
+    volatile s32 timeout;
+
+    timeout = 0x100000;
+    if (*D_80083198 & 0x20000000) {
+        do {
+            if (--timeout == -1) {
+                func_80064424(D_800132E4);
+                return -1;
+            }
+        } while (*D_80083198 & 0x20000000);
+    }
+    return 0;
+}
+
+s32 func_8006438C(void) {
+    volatile s32 timeout;
+
+    timeout = 0x100000;
+    if (*D_80083178 & 0x01000000) {
+        do {
+            if (--timeout == -1) {
+                func_80064424(D_800132F4);
+                return -1;
+            }
+        } while (*D_80083178 & 0x01000000);
+    }
+    return 0;
+}
+
+s32 func_80064424(u8 *arg0) {
+    u32 status;
+    register s32 ret asm("$2");
+
+    func_8001674C(D_80013364, arg0);
+    status = *D_80083198;
+    func_8001674C(D_80013304, (*D_8008316C >> 24) & 1, (*D_80083178 >> 24) & 1, *D_80083164, *D_80083170);
+    func_8001674C(D_8001332C,
+                  (~status >> 31) & 1,
+                  (status >> 30) & 1,
+                  (status >> 29) & 1,
+                  (status >> 28) & 1,
+                  (status >> 27) & 1,
+                  (status >> 25) & 1,
+                  (status >> 23) & 1);
+
+    *D_80083198 = 0x80000000;
+    *D_8008316C = 0;
+    *D_80083178 = 0;
+
+    asm volatile(
+        "addu  $2,$0,$0\n"
+        "lui   $3,%%hi(D_80083178)\n"
+        "lw    $3,%%lo(D_80083178)($3)\n"
+        "lui   $4,%%hi(D_80083198)\n"
+        "lw    $4,%%lo(D_80083198)($4)\n"
+        "lw    $3,0($3)\n"
+        "lui   $3,0x6000\n"
+        "sw    $3,0($4)"
+        : "=r"(ret)
+        :
+        : "$3", "$4", "memory");
+
+    return ret;
 }
