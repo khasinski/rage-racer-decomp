934688c4b4dc9814da9aaa4a09c1b2ebaf2aa6ab
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 00:55:59 +0200

    Decompile fixed point blend helper

--- src/main/PAL/main/func_80046598.c
+++ src/main/PAL/main/func_80046598.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+// Fixed-point blend in 0..10000 scale.
+s32 func_80046598(s32 arg0, s32 arg1) {
+    register s32 value asm("$2");
+    register s32 doubled asm("$3");
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80046598", func_80046598);
-
+    value = 0x2710 - arg0;
+    doubled = arg0 * 2;
+    value = (value * doubled) / 10000;
+    return ((value * arg1) + (arg0 * arg0)) / 10000;
+}
