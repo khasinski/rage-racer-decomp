b6ecb2404ad37e355d5055c5889fdd4be8e7f313
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-05 18:27:35 +0200

    Group libsnd close wrappers

--- src/main/PAL/lib/libsnd/SsSepCloseWrapper.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-void SsSeqClose(s16 arg0) asm("func_80071948");
-
-void SsSepCloseWrapper(s16 arg0) asm("func_80071AE8");
-
-void SsSepCloseWrapper(s16 arg0) {
-    SsSeqClose(arg0);
-}
--- src/main/PAL/lib/libsnd/SsSeqCloseWrapper.c
+++ src/main/PAL/lib/libsnd/SsSeqCloseWrapper.c
@@ -7,3 +7,9 @@ void SsSeqCloseWrapper(s16 arg0) asm("func_80071AC4");
 void SsSeqCloseWrapper(s16 arg0) {
     SsSeqClose(arg0);
 }
+
+void SsSepCloseWrapper(s16 arg0) asm("func_80071AE8");
+
+void SsSepCloseWrapper(s16 arg0) {
+    SsSeqClose(arg0);
+}
