58dd2631bf2bfebc14d20669a6a0b6a0897f729e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 13:38:10 +0200

    Decompile GPU DMA transfer wait

--- src/main/PAL/main/func_80066D84.c
+++ src/main/PAL/main/func_80066D84.c
@@ -1,5 +1,45 @@
 #include "common.h"
 
+extern volatile u32 *D_800942CC;
+extern volatile u32 *D_800942D0;
+extern volatile u32 *D_800942D4;
+extern volatile u32 *D_800942D8;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80066D84", func_80066D84);
+void func_80067F04(void);
+s32 func_80067F38(void);
 
+s32 func_80066D84(u32 *arg0, s32 arg1) {
+    register s32 size asm("$16");
+    register u32 mask asm("$17");
+    register volatile u32 *status asm("$5");
+    register s32 offset asm("$2");
+
+    size = arg1;
+    status = D_800942D8;
+    *status |= 0x08000000;
+    *D_800942D4 = 0;
+    __asm__ volatile("" ::: "memory");
+    offset = (size << 2) - 4;
+    arg0 = (u32 *)((u8 *)arg0 + offset);
+    *D_800942CC = (u32)arg0;
+    *D_800942D0 = size;
+    *D_800942D4 = 0x11000002;
+    func_80067F04();
+
+    if ((*D_800942D4 & 0x01000000) != 0) {
+        mask = 0x01000000;
+        while (1) {
+            register u32 statusValue asm("$2");
+
+            if (func_80067F38() != 0) {
+                return -1;
+            }
+            statusValue = *D_800942D4;
+            statusValue &= mask;
+            if (statusValue == 0) {
+                break;
+            }
+        }
+    }
+    return size;
+}
