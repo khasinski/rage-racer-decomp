e289c295fbde8d8971bf6e1df8800cdac126cd20
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 03:06:57 +0200

    Decompile reverb depth helper

--- src/main/PAL/main/func_8005B070.c
+++ src/main/PAL/main/func_8005B070.c
@@ -1,7 +1,32 @@
 #include "common.h"
+#include "psyq/snd.h"
 
+extern s32 D_801E6D84;
+extern s32 D_801E6D88;
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B070);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B190);
+
+void func_8005B190(s32 left, s32 right) {
+    if (left >= 0) {
+        if (left >= 0x80) {
+            left = 0x7F;
+        }
+    } else {
+        left = 0;
+    }
+
+    if (right >= 0) {
+        if (right >= 0x80) {
+            right = 0x7F;
+        }
+    } else {
+        right = 0;
+    }
+
+    D_801E6D84 = left;
+    D_801E6D88 = right;
+    SsUtSetReverbDepth((s16)left, (s16)right);
+}
+
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B204);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B2F0);
