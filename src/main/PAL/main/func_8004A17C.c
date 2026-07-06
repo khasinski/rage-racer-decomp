746ff8a7fffcbba749846172ff069b850c55ec49
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 11:59:46 +0200

    Decompile func_8004A17C

--- src/main/PAL/main/func_8004A17C.c
+++ src/main/PAL/main/func_8004A17C.c
@@ -1,5 +1,33 @@
 #include "common.h"
 
+extern s32 D_8009B280;
+extern u8 D_8009B284;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8004A17C", func_8004A17C);
+void func_80047024(void *arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8);
 
+void func_8004A17C(s32 delta) {
+    u8 *scratch;
+    register s32 value asm("$2");
+    s32 limit;
+    s32 alpha;
+
+    scratch = *(u8 **)0x1F800004;
+    if (delta > 0) {
+        value = D_8009B280 + delta;
+        D_8009B280 = value;
+        if (0xFFFF < value) {
+            D_8009B280 = 0xFFFF;
+        }
+    } else {
+        value = D_8009B280 + delta;
+        D_8009B280 = value;
+        if (value < 0) {
+            D_8009B280 = 0;
+        }
+    }
+
+    limit = 0x1E0;
+    D_8009B284 = D_8009B280 >> 8;
+    alpha = D_8009B284;
+    func_80047024(scratch + 0x18, 0x48, 0, 0xF8, limit, alpha, alpha, alpha, 0x40);
+}
