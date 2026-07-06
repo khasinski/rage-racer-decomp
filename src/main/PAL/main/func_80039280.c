fb5243b3273c7eb23e18499287ba58a204028aaa
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 09:49:37 +0200

    Type car oscillation update

--- src/main/PAL/main/func_80039280.c
+++ src/main/PAL/main/func_80039280.c
@@ -1,9 +1,10 @@
 #include "common.h"
+#include "game/car.h"
 
-s32 func_80039184(u8 *arg0);
+s32 func_80039184(GameCarRuntime *arg0);
 
-void func_80039280(u8 *arg0) {
-    register u8 *obj asm("$16");
+void func_80039280(GameCarRuntime *arg0) {
+    register GameCarRuntime *obj asm("$16");
     register s32 value asm("$4");
     register s32 temp asm("$3");
     register s32 result asm("$2");
@@ -13,13 +14,13 @@ void func_80039280(u8 *arg0) {
     obj = arg0;
     (void)stack;
 
-    if (*(s16 *)(obj + 0x98) != 0) {
-        result = *(s16 *)(obj + 0x9A);
+    if (obj->field_98 != 0) {
+        result = obj->field_9A;
         value = result * result;
         asm volatile("" : "=r"(value) : "0"(value));
         result = 0x2AAAAAAB;
         asm volatile("mult $4,$2");
-        temp = *(s16 *)(obj + 0x90);
+        temp = obj->field_90;
         asm volatile("sra $4,$4,31\nmfhi $2\nsubu $4,$2,$4" : "=r"(value), "=r"(result) : "0"(value), "1"(result));
         asm volatile("" : "=r"(value) : "0"(value));
         result = temp;
@@ -28,51 +29,51 @@ void func_80039280(u8 *arg0) {
             value >>= 8;
         }
         result += value;
-        *(s16 *)(obj + 0x90) = result;
+        obj->field_90 = result;
 
         temp = value;
         if (value < 0) {
             temp = value + 3;
         }
-        result = *(u16 *)(obj + 0x94);
+        result = (u16)obj->field_94;
         temp >>= 2;
         result += temp;
-        *(s16 *)(obj + 0x94) = result;
+        obj->field_94 = result;
 
-        result = *(s16 *)(obj + 0x90);
-        temp = *(s16 *)(obj + 0x94);
-        *(s32 *)(obj + 0x20) = result;
-        *(s32 *)(obj + 0x28) = temp;
+        result = obj->field_90;
+        temp = obj->field_94;
+        obj->field_20 = result;
+        obj->field_28 = temp;
         return;
     }
 
     asm("" : "=r"(obj) : "0"(obj));
-    value = func_80039184((u8 *)obj);
+    value = func_80039184(obj);
     if (value == 0) {
         return;
     }
 
     one = 1;
-    *(s16 *)(obj + 0x98) = one;
+    obj->field_98 = one;
     if (value > 0) {
-        temp = value * *(s32 *)(obj + 0xA4);
+        temp = value * obj->field_A4;
         temp = temp / -4800;
-        *(s16 *)(obj + 0x98) = one;
-        *(s16 *)(obj + 0x9C) = temp;
+        obj->field_98 = one;
+        obj->field_9C = temp;
         goto finish_setup;
     } else {
         result = 2;
-        *(s16 *)(obj + 0x98) = result;
+        obj->field_98 = result;
         result = -value;
-        *(s16 *)(obj + 0x9C) = result;
+        obj->field_9C = result;
     }
 
 finish_setup:
-    result = *(u16 *)(obj + 0x20);
-    temp = *(u16 *)(obj + 0x28);
-    value = *(u16 *)(obj + 4);
-    *(s16 *)(obj + 0x9A) = 0;
-    *(s16 *)(obj + 0x90) = result;
-    *(s16 *)(obj + 0x94) = temp;
-    *(s16 *)(obj + 0x9E) = value;
+    result = (u16)obj->field_20;
+    temp = (u16)obj->field_28;
+    value = (u16)obj->y;
+    obj->field_9A = 0;
+    obj->field_90 = result;
+    obj->field_94 = temp;
+    obj->field_9E = value;
 }
