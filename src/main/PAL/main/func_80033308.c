0a2105d78a37d06024959c7faa371872b9e50d66
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 05:21:32 +0200

    Decompile primitive state helper

--- src/main/PAL/main/func_80033308.c
+++ src/main/PAL/main/func_80033308.c
@@ -1,5 +1,50 @@
 #include "common.h"
 
+extern u8 *D_8019C900;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80033308", func_80033308);
+void AddPrim(void *ot, void *prim) asm("func_80064DDC");
 
+void func_80033308(s32 arg0, s32 arg1) {
+    register u8 *base __asm("$16");
+    register u8 *prim __asm("$17");
+    register s32 mode __asm("$18");
+    register s32 firstOffset __asm("$5");
+    register s32 value __asm("$4");
+    register s32 temp __asm("$2");
+    register u8 *ot __asm("$4");
+
+    mode = arg1;
+    firstOffset = 0x237AC;
+    temp = 0x237C0;
+    value = arg0 << 3;
+    base = D_8019C900;
+    value += 0x50;
+    prim = base + temp;
+
+    *(base + 0x237B8) = value;
+    AddPrim(D_8019C900 + 0xCC, base + firstOffset);
+    firstOffset = (s32)prim;
+
+    if (mode > 0) {
+        *(volatile u8 *)(base + 0x237CC) = 0x88;
+        __asm__ volatile("" : : : "memory");
+        ot = D_8019C900;
+        __asm__ volatile("" : : "r"(ot));
+        temp = 0x7810;
+        goto add_second;
+    }
+
+    if (mode >= 0) {
+        return;
+    }
+
+    *(volatile u8 *)(base + 0x237CC) = 0x78;
+    __asm__ volatile("" : : : "memory");
+    ot = D_8019C900;
+    __asm__ volatile("" : : "r"(ot));
+    temp = 0x780F;
+
+add_second:
+    *(u16 *)(base + 0x237CE) = temp;
+    AddPrim(ot + 0xCC, (void *)firstOffset);
+}
