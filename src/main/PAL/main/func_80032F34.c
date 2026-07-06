a5c27219eaff1c75f2c4fc351e7c808605cb59cc
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 04:48:10 +0200

    Decompile tile primitive helper

--- src/main/PAL/main/func_80032F34.c
+++ src/main/PAL/main/func_80032F34.c
@@ -1,5 +1,23 @@
 #include "common.h"
 
+void func_80064FF8(u8 *prim);
+void func_80064DDC(void *ot, void *prim);
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80032F34", func_80032F34);
+u8 *func_80032F34(void *ot, u8 *prim, s32 x, s32 y, s32 w, s32 h, s32 r, s32 g, s32 b) {
+    u8 *oldPrim;
 
+    func_80064FF8(prim);
+
+    oldPrim = prim;
+    *(s16 *)(prim + 0x08) = x;
+    *(s16 *)(prim + 0x0A) = y;
+    *(s16 *)(prim + 0x0C) = w;
+    *(s16 *)(prim + 0x0E) = h;
+    prim[0x04] = r;
+    prim[0x05] = g;
+    prim[0x06] = b;
+
+    prim += 0x10;
+    func_80064DDC(ot, oldPrim);
+    return prim;
+}
