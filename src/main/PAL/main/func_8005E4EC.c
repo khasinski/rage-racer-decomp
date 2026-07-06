e9306bbb41ac0f512b374ec1037910a684352320
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 03:41:08 +0200

    Decompile sequence VAB slot loader

--- src/main/PAL/main/func_8005E4EC.c
+++ src/main/PAL/main/func_8005E4EC.c
@@ -2,16 +2,67 @@
 
 extern s16 D_801E6CA8[];
 extern s16 D_8009B510[];
+extern s32 D_800125EC[];
+extern s32 D_8009E68C;
 extern s32 D_801E6C9C;
 extern s16 D_801E6D90;
+extern s32 D_801E6D9C;
+extern s32 D_801F17B4;
+extern char D_80012788[];
+extern char D_800127A0[];
 
+void func_8001674C(char *arg0);
+void func_80063D9C(s32 arg0);
+s32 func_8006F004(s32 arg0);
 void func_80077C7C(s16 arg0, s16 arg1, s16 arg2, s16 arg3, s16 arg4, s16 arg5, s16 arg6, s16 arg7);
+s32 func_8007317C(s32 arg0);
+s32 func_800730BC(s32 arg0, s32 arg1);
+s32 func_80072C4C(s32 arg0, s32 arg1, s32 arg2);
 void func_80073748(s32 arg0, s32 arg1);
 void func_8007865C(s32 arg0);
 void func_80071AC4(s32 arg0);
 void func_80072B3C(s32 arg0);
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E4EC);
+s32 func_8005E4EC(s32 slot, s32 header, s32 body, s32 seq) {
+    register s32 slotReg asm("$16") = slot;
+    register s32 bodyReg asm("$18") = body;
+    register s32 seqReg asm("$19") = seq;
+    register s16 *vabIdPtr asm("$16");
+    register s32 currentVabId asm("$5");
+    register s32 fail asm("$17");
+    register s32 ret asm("$2");
+
+    D_8009E68C = slotReg;
+    ret = func_80072C4C(header, -1, D_800125EC[slotReg]);
+    {
+        register s16 *vabIdBase asm("$3") = D_801E6CA8;
+        slotReg *= 2;
+        vabIdPtr = (s16 *)((s32)vabIdBase + slotReg);
+    }
+    *vabIdPtr = ret;
+    asm volatile("" : "=r"(ret) : "0"(ret));
+
+    currentVabId = (s16)ret;
+    fail = -1;
+    if (currentVabId == fail) {
+        func_8001674C(D_80012788);
+        func_80063D9C(1);
+    }
+
+    ret = func_800730BC(bodyReg, currentVabId);
+    *vabIdPtr = ret;
+    currentVabId = (s16)ret;
+    if (currentVabId == fail) {
+        func_8001674C(D_800127A0);
+        func_80063D9C(1);
+    }
+
+    *(s32 *)&D_801E6D90 = (s16)func_8006F004(seqReg);
+    D_801E6D9C = 0;
+    ret = func_8007317C(0);
+    D_801F17B4 = (s16)ret;
+    return D_801F17B4;
+}
 
 s32 func_8005E600(s32 slot) {
     register s32 slotReg asm("$17") = slot;
