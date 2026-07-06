5b0808032b430ea54e8a7e46c97b32810a4f61d1
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 06:08:50 +0200

    Decompile reverb utility wrappers

--- src/main/PAL/lib/libsnd/SsUtSetReverbType.c
+++ src/main/PAL/lib/libsnd/SsUtSetReverbType.c
@@ -1,7 +1,35 @@
 #include "common.h"
+#include "psyq/spu.h"
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsUtSetReverbType", SsUtSetReverbType);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsUtSetReverbType", SsUtGetReverbType);
+
+extern SpuReverbAttr D_8019C778;
+extern s16 D_8019C77C;
+
+s32 SsUtGetReverbType(void) asm("func_800736B8");
+s32 SsUtGetReverbType(void) {
+    return D_8019C77C;
+}
+
+void SsUtReverbOn(void) asm("func_800736C8");
+void SsUtReverbOn(void) {
+    SpuSetReverb(1);
+}
+
+void SsUtReverbOff(void) asm("func_800736E8");
+void SsUtReverbOff(void) {
+    SpuSetReverb(0);
+}
+
+asm(".globl func_800736F8\nfunc_800736F8 = func_800736E8 + 0x10");
+
+void SsUtSetReverbFeedback(s32 feedback) asm("func_80073708");
+void SsUtSetReverbFeedback(s32 feedback) {
+    D_8019C778.mask = 0x10;
+    D_8019C778.feedback = (s16)feedback;
+    SpuSetReverbModeParam(&D_8019C778);
+}
+
 INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsUtSetReverbType", SsUtSetReverbDepth);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsUtSetReverbType", SsUtSetReverbDelay);
