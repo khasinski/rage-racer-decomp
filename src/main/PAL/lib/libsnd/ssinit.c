b92aa7f773ab9f41c67b7b7ca91ff5344bca4680
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 06:17:59 +0200

    Decompile sound init wrapper

--- src/main/PAL/lib/libsnd/ssinit.c
+++ src/main/PAL/lib/libsnd/ssinit.c
@@ -1,5 +1,15 @@
 #include "common.h"
+#include "psyq/kernel.h"
 
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/ssinit", ssinit);
+void SpuInit(void) asm("func_80078838");
+void _SsInitTables(void) asm("func_80071B0C");
 
+void ssinit(void) asm("func_80071BF4");
+void ssinit(void) {
+    KernelCallbackSlot3();
+    SpuInit();
+    _SsInitTables();
+}
+
+asm(".globl ssinit\nssinit = func_80071BF4");
