3e676ded1d5efbe4c533c44076a9eb7f0d3a0072
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 04:13:27 +0200

    Decompile CD event table helpers

--- src/main/PAL/main/func_8005EFAC.c
+++ src/main/PAL/main/func_8005EFAC.c
@@ -1,7 +1,42 @@
 #include "common.h"
+#include "psyq/kernel.h"
+
+extern s32 D_8009B538[];
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005EFAC", func_8005EFAC);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005EFAC", func_8005F0D4);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005EFAC", func_8005F16C);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005EFAC", func_8005F204);
+
+void func_8005F0D4(void) {
+    EnableEvent(D_8009B538[0]);
+    EnableEvent(D_8009B538[1]);
+    EnableEvent(D_8009B538[2]);
+    EnableEvent(D_8009B538[3]);
+    EnableEvent(D_8009B538[4]);
+    EnableEvent(D_8009B538[5]);
+    EnableEvent(D_8009B538[6]);
+    EnableEvent(D_8009B538[7]);
+}
+
+void func_8005F16C(void) {
+    DisableEvent(D_8009B538[0]);
+    DisableEvent(D_8009B538[1]);
+    DisableEvent(D_8009B538[2]);
+    DisableEvent(D_8009B538[3]);
+    DisableEvent(D_8009B538[4]);
+    DisableEvent(D_8009B538[5]);
+    DisableEvent(D_8009B538[6]);
+    DisableEvent(D_8009B538[7]);
+}
+
+void func_8005F204(void) {
+    EnterCriticalSection();
+    CloseEvent(D_8009B538[0]);
+    CloseEvent(D_8009B538[1]);
+    CloseEvent(D_8009B538[2]);
+    CloseEvent(D_8009B538[3]);
+    CloseEvent(D_8009B538[4]);
+    CloseEvent(D_8009B538[5]);
+    CloseEvent(D_8009B538[6]);
+    CloseEvent(D_8009B538[7]);
+    ExitCriticalSection();
+}
