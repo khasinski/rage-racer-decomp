69719377ee486e63b074d929cd0ba6066f1f83de
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 04:09:56 +0200

    Decompile memory card format helper

--- src/main/PAL/main/func_8005ECE0.c
+++ src/main/PAL/main/func_8005ECE0.c
@@ -1,6 +1,31 @@
 #include "common.h"
 
+extern char D_800127CC[];
+
+void func_800632F0(void *dst, void *fmt, s32 arg0, s32 arg1);
+void func_8005F304(void);
+s32 func_80063280(void *device);
+s32 func_8005F55C(void);
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005ECE0_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005ECE0);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005ECE0", func_8005EF44);
+
+s32 func_8005EF44(s32 arg0, s32 arg1) {
+    char device[8];
+    register s32 status asm("$3");
+
+    func_800632F0(device, D_800127CC, arg0, arg1);
+    func_8005F304();
+    func_80063280(device);
+    status = func_8005F55C();
+
+    if (status != 1) {
+        if (status == 3) {
+            status = -1;
+        } else {
+            status = -3;
+        }
+    }
+
+    return status;
+}
