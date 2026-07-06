cce2c962635bbc7d6350737966b06cebf3abb01b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 02:11:49 +0200

    Decompile in-place matrix GTE transform helpers

--- src/main/PAL/main/func_80069568.c
+++ src/main/PAL/main/func_80069568.c
@@ -1,5 +1,77 @@
 #include "common.h"
 
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80069568", func_80069568);
+void *func_80069568(s32 *matrix, void *inout) {
+    register s32 m0 asm("$8") = matrix[0];
+    register s32 m1 asm("$9") = matrix[1];
+    register s32 m2 asm("$10") = matrix[2];
+    register s32 m3 asm("$11") = matrix[3];
+    register s32 m4 asm("$12") = matrix[4];
 
+    asm volatile(
+        "ctc2 $8,$0\n"
+        "ctc2 $9,$1\n"
+        "ctc2 $10,$2\n"
+        "ctc2 $11,$3\n"
+        "ctc2 $12,$4\n"
+        "lhu $8,0($5)\n"
+        "lw $9,4($5)\n"
+        "lw $10,12($5)\n"
+        "lui $1,0xFFFF\n"
+        "and $9,$9,$1\n"
+        "or $8,$8,$9\n"
+        "mtc2 $8,$0\n"
+        "mtc2 $10,$1\n"
+        "nop\n"
+        "cop2 0x486012\n"
+        "lhu $8,2($5)\n"
+        "lw $9,8($5)\n"
+        "lh $10,14($5)\n"
+        "sll $9,$9,16\n"
+        "or $8,$8,$9\n"
+        "mfc2 $11,$9\n"
+        "mfc2 $12,$10\n"
+        "mfc2 $13,$11\n"
+        "mtc2 $8,$0\n"
+        "mtc2 $10,$1\n"
+        "nop\n"
+        "cop2 0x486012\n"
+        "lhu $8,4($5)\n"
+        "lw $9,8($5)\n"
+        "lw $10,16($5)\n"
+        "lui $1,0xFFFF\n"
+        "and $9,$9,$1\n"
+        "or $8,$8,$9\n"
+        "mfc2 $14,$9\n"
+        "mfc2 $15,$10\n"
+        "mfc2 $24,$11\n"
+        "mtc2 $8,$0\n"
+        "mtc2 $10,$1\n"
+        "nop\n"
+        "cop2 0x486012\n"
+        "andi $11,$11,0xFFFF\n"
+        "sll $14,$14,16\n"
+        "or $14,$14,$11\n"
+        "sw $14,0($5)\n"
+        "andi $13,$13,0xFFFF\n"
+        "sll $24,$24,16\n"
+        "or $24,$24,$13\n"
+        "sw $24,12($5)\n"
+        "mfc2 $8,$9\n"
+        "mfc2 $9,$10\n"
+        "andi $8,$8,0xFFFF\n"
+        "sll $12,$12,16\n"
+        "or $8,$8,$12\n"
+        "sw $8,4($5)\n"
+        "andi $15,$15,0xFFFF\n"
+        "sll $9,$9,16\n"
+        "or $9,$9,$15\n"
+        "sw $9,8($5)\n"
+        "swc2 $11,16($5)"
+        :
+        : "r"(m0), "r"(m1), "r"(m2), "r"(m3), "r"(m4), "r"(inout)
+        : "memory");
+    asm volatile("move $2,$5");
+}
+
+asm(".word 0");
