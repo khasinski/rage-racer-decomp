47ccbcc2408d860c0bacbd814f613e3c7899f379
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 03:12:33 +0200

    Decompile sound slot voice trigger

--- src/main/PAL/main/func_8005B070.c
+++ src/main/PAL/main/func_8005B070.c
@@ -4,6 +4,11 @@
 extern s32 D_801E6D84;
 extern s32 D_801E6D88;
 extern s32 D_801E6D80;
+extern s16 D_801E6CA8[];
+extern s16 D_80082F28[];
+
+void func_80077C7C(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7);
+void GamePlaySoundSlotVoice(s32 slot, s32 tone, s32 vabSlot) asm("func_8005B2F0");
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B070);
 
@@ -68,4 +73,12 @@ void func_8005B204(s32 type, s32 left, s32 right) {
     }
 }
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B2F0);
+void GamePlaySoundSlotVoice(s32 slot, s32 tone, s32 vabSlot) {
+    register s16 *base asm("$3") = D_80082F28;
+    register s16 *row asm("$2");
+    register s16 *entry asm("$3");
+
+    row = (s16 *)((s32)base + (slot << 2));
+    entry = (s16 *)((tone << 1) + (s32)row);
+    func_80077C7C((s16)(slot + 0xE), D_801E6CA8[(s16)vabSlot], *entry, 0, 0x3C, 0, 0, 0);
+}
