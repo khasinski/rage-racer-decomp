f75c89fbebf45bdb448a763e3b10077ab347cb2d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 03:10:25 +0200

    Decompile reverb setup helper

--- src/main/PAL/main/func_8005B070.c
+++ src/main/PAL/main/func_8005B070.c
@@ -3,6 +3,7 @@
 
 extern s32 D_801E6D84;
 extern s32 D_801E6D88;
+extern s32 D_801E6D80;
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B070);
 
@@ -28,5 +29,43 @@ void func_8005B190(s32 left, s32 right) {
     SsUtSetReverbDepth((s16)left, (s16)right);
 }
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B204);
+void func_8005B204(s32 type, s32 left, s32 right) {
+    s32 tempLeft;
+    s32 tempRight;
+
+    if (left >= 0) {
+        tempLeft = left;
+        if (tempLeft >= 0x80) {
+            tempLeft = 0x7F;
+        }
+    } else {
+        tempLeft = 0;
+    }
+
+    if ((left = tempLeft, right) >= 0) {
+        tempRight = right;
+        if (tempRight >= 0x80) {
+            tempRight = 0x7F;
+        }
+    } else {
+        tempRight = 0;
+    }
+    right = tempRight;
+
+    SsUtReverbOff();
+
+    if ((u32)(type - 1) < 9) {
+        D_801E6D80 = type;
+        D_801E6D84 = left;
+        D_801E6D88 = right;
+        SsUtSetReverbType((s16)type);
+        SsUtReverbOn();
+        func_8005B190(left, right);
+    } else {
+        D_801E6D80 = 0;
+        D_801E6D88 = 0;
+        D_801E6D84 = 0;
+    }
+}
+
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005B070", func_8005B2F0);
