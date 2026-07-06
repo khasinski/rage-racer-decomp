c02ef4e2a05e6f90e52999c67f69e2e99c9e986f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 08:38:00 +0200

    Clean up AddPrim symbol aliases

--- src/main/PAL/main/AddPrim.c
+++ src/main/PAL/main/AddPrim.c
@@ -1,7 +1,6 @@
 #include "common.h"
 
-void AddPrim(u32 *arg0, u32 *arg1) asm("func_80064DDC");
-void AddPrim(u32 *arg0, u32 *arg1) {
+void func_80064DDC(u32 *arg0, u32 *arg1) {
     register u32 mask asm("$6") = 0x00FFFFFF;
     register u32 tag asm("$7") = 0xFF000000;
 
@@ -9,8 +8,7 @@ void AddPrim(u32 *arg0, u32 *arg1) {
     *arg0 = (*arg0 & tag) | ((u32)arg1 & mask);
 }
 
-void AddPrims(u32 *arg0, u32 arg1, u32 *arg2) asm("func_80064E18");
-void AddPrims(u32 *arg0, u32 arg1, u32 *arg2) {
+void func_80064E18(u32 *arg0, u32 arg1, u32 *arg2) {
     register u32 mask asm("$7") = 0x00FFFFFF;
     register u32 tag asm("$8") = 0xFF000000;
 
