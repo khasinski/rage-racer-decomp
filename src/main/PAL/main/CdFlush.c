b149918a6d8ce9e1a5f3a4109460fc724b849924
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 02:31:58 +0200

    Decompile CD callback helpers

--- src/main/PAL/main/CdFlush.c
+++ src/main/PAL/main/CdFlush.c
@@ -2,11 +2,15 @@
 #include "psyq/cd.h"
 
 extern s32 D_80099048;
+extern s32 D_8009903C;
+extern s32 D_80099040;
 extern char *D_80099060[];
 extern char *D_800990E0[];
 extern u8 D_800136D0[];
 
 void CD_flush(void) asm("func_8006BAF0");
+s32 func_8006B0D4(s32 arg0, s32 arg1);
+s32 func_8006B354(s32 arg0, s32 arg1);
 
 void CdFlush(void) asm("func_8006A494");
 void CdFlush(void) {
@@ -38,4 +42,24 @@ char *CdIntstr(s32 arg0) {
     return D_800990E0[arg0];
 }
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8006A494", func_8006A534);
+s32 func_8006A534(s32 arg0, s32 arg1) {
+    return func_8006B0D4(arg0, arg1);
+}
+
+s32 func_8006A554(s32 arg0, s32 arg1) {
+    return func_8006B354(arg0, arg1);
+}
+
+s32 func_8006A574(s32 arg0) {
+    s32 old = D_8009903C;
+
+    D_8009903C = arg0;
+    return old;
+}
+
+s32 func_8006A58C(s32 arg0) {
+    s32 old = D_80099040;
+
+    D_80099040 = arg0;
+    return old;
+}
