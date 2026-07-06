33a3cacb8a34f1340dd3c24bb0567483d41c6b95
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 09:59:50 +0200

    Type car runtime render entrypoint

--- src/main/PAL/main/func_80038A88.c
+++ src/main/PAL/main/func_80038A88.c
@@ -1,9 +1,10 @@
 #include "common.h"
+#include "game/car.h"
 
-extern u8 D_801F1854[];
+extern GameCarRuntime D_801F1854[];
 
 void func_80017A10(s32 arg0);
-void func_8001DFC0(void *arg0);
+void func_8001DFC0(GameCarRuntime *arg0);
 
 void func_80038A88(void) {
     func_80017A10(1);
