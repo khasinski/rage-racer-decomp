2fa2532999fd08dd5caea172f221dfb6371705df
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 11:18:31 +0200

    Decompile func_8006E4E4

--- src/main/PAL/main/func_8006E4E4.c
+++ src/main/PAL/main/func_8006E4E4.c
@@ -1,5 +1,38 @@
-#include "common.h"
+#include "psyq/kernel.h"
 
+extern u16 D_80099430[];
+extern u16 D_80099462;
+extern u32 D_80099464;
+extern volatile u16 *D_8009A4BC;
+extern volatile u16 *D_8009A4C0;
+extern volatile u32 *D_8009A4C4;
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8006E4E4", func_8006E4E4);
+void *func_8006E4E4(void) {
+    u16 *state;
+    register volatile u16 *mask asm("$2");
+    register volatile u32 *status asm("$4");
+    volatile u16 *clearMask;
+    u32 statusValue;
 
+    state = D_80099430;
+    if (state[0] == 0) {
+        return 0;
+    }
+
+    EnterCriticalSection();
+
+    mask = D_8009A4C0;
+    status = D_8009A4C4;
+    D_80099462 = *mask;
+    statusValue = *status;
+    clearMask = D_8009A4BC;
+    D_80099464 = statusValue;
+    *mask = 0;
+    *clearMask = *mask;
+    status = D_8009A4C4;
+    *status &= 0x77777777;
+    ResetEntryInt();
+
+    state[0] = 0;
+    return state;
+}
