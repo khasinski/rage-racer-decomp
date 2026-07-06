3008bf951a66655fbc1323feab548cfe669b374e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 03:33:51 +0200

    Decompile VAB slot voice start

--- src/main/PAL/main/func_8005E4EC.c
+++ src/main/PAL/main/func_8005E4EC.c
@@ -1,6 +1,17 @@
 #include "common.h"
 
+extern s16 D_801E6CA8[];
+extern s16 D_8009B510[];
+
+void func_80077C7C(s16 arg0, s16 arg1, s16 arg2, s16 arg3, s16 arg4, s16 arg5, s16 arg6, s16 arg7);
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E4EC);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E600);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_8005E4EC", func_8005E694);
+
+void func_8005E694(s32 voice, s32 unused, s16 vabSlot) {
+    register s32 voiceOffset asm("$2") = voice * 4;
+    register s32 vabOffset asm("$6") = (s16)vabSlot * 2;
+
+    asm("" : "=r"(voiceOffset) : "0"(voiceOffset));
+    func_80077C7C((s16)voice, *(s16 *)((s32)D_801E6CA8 + vabOffset), *(s16 *)((s32)D_8009B510 + voiceOffset), 0, 0x3C, 0, 0, 0);
+}
