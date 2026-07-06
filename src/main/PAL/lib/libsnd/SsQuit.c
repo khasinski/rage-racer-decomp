63143b1fe210309b85928d3217ab938eeae481a9
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 05:58:31 +0200

    Decompile sound shutdown wrappers

--- src/main/PAL/lib/libsnd/SsQuit.c
+++ src/main/PAL/lib/libsnd/SsQuit.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsQuit", SsQuit);
+void SsQuit(void) asm("func_80071C24");
+void SsQuit(void) {
+    void SpuQuit(void) asm("func_80079AE4");
 
+    SpuQuit();
+}
--- src/main/PAL/lib/libsnd/SsVabTransBody.c
+++ src/main/PAL/lib/libsnd/SsVabTransBody.c
@@ -2,4 +2,10 @@
 
 
 INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsVabTransBody", SsVabTransBody);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/lib/libsnd/SsVabTransBody", SsVabTransCompleted);
+
+s16 SsVabTransCompleted(s16 immediate_flag) asm("func_8007317C");
+s16 SsVabTransCompleted(s16 immediate_flag) {
+    s32 SpuIsTransferCompleted(s32 wait) asm("func_8007B1EC");
+
+    return SpuIsTransferCompleted(immediate_flag);
+}
