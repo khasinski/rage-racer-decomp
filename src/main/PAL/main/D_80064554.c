d21e0f1466c564c620ba7513ba14efd7959dfe6e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-07 20:55:59 +0200

    Restore assembly stubs

--- src/main/PAL/main/D_80064554.c
+++ src/main/PAL/main/D_80064554.c
@@ -1,22 +1,5 @@
 #include "common.h"
 
 
-asm(
-    ".set noreorder\n"
-    ".set noat\n"
-    ".globl D_80064554\n"
-    "D_80064554:\n"
-    ".word 0x00FFFFFF\n"
-    "lui $8,0x8006\n"
-    "addiu $8,$8,0x4554\n"
-    "addi $1,$4,-1\n"
-    ".word 0x18200004\n"
-    "lw $2,0($8)\n"
-    "sll $1,$4,1\n"
-    "jr $31\n"
-    "sw $1,0($8)\n"
-    "1:\n"
-    "lui $1,0x00FF\n"
-    "ori $1,$1,0xFFFF\n"
-    "jr $31\n"
-    "sw $1,0($8)");
+INCLUDE_ASM("asm/PAL/main/nonmatchings/main/D_80064554", D_80064554);
+
