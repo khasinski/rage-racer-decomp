063627958603c50fed4db8832c82b548207da237
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-08 11:49:55 +0200

    Decompile CD state step helpers

--- src/main/PAL/main/func_800432A8.c
+++ src/main/PAL/main/func_800432A8.c
@@ -1,6 +1,67 @@
 #include "common.h"
 
+extern s32 D_8007F604;
+extern s32 D_8007F60C;
+
+s32 func_8006A534(s32 arg0, s32 arg1);
+s32 func_8006A5A4(s32 arg0, void *arg1, s32 arg2);
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_800432A8", func_800432A8_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800432A8", func_800432A8);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800432A8", func_80043494);
+
+void func_80043494(void) {
+    s32 state;
+    s32 status;
+
+    state = D_8007F60C;
+    if (state == 1) {
+        goto state_1;
+    }
+    if (state >= 2) {
+        goto state_ge_2;
+    }
+    if (state == 0) {
+        goto state_0;
+    }
+    goto done;
+
+state_ge_2:
+    if (state == 2) {
+        goto state_2;
+    }
+    if (state == 3) {
+        goto state_3;
+    }
+    goto done;
+
+state_0:
+    if (func_8006A534(1, 0) == 0) {
+        goto done;
+    }
+    D_8007F60C = 1;
+state_1:
+    if (func_8006A5A4(3, 0, 0) == 0) {
+        goto done;
+    }
+    D_8007F60C = 2;
+    goto done;
+
+state_2:
+    status = func_8006A534(1, 0);
+    if (status == state) {
+        D_8007F60C = 3;
+        goto done;
+    }
+    if (status == 5) {
+        D_8007F60C = 1;
+        goto done;
+    }
+    goto done;
+
+state_3:
+    D_8007F604 = -1;
+    D_8007F60C = 0;
+
+done:
+    return;
+}
--- src/main/PAL/main/func_80043598.c
+++ src/main/PAL/main/func_80043598.c
@@ -17,6 +17,7 @@ extern s32 D_801E42E4;
 extern CdlLOC D_8007F5B0[];
 
 s32 func_8006A5A4(s32 arg0, void *arg1, s32 arg2);
+s32 func_8006A534(s32 arg0, s32 arg1);
 s32 func_8006A554(s32 arg0, void *arg1);
 s32 CdPosToInt_Local(CdlLOC *loc) asm("func_8006AADC");
 void func_800432A8(void);
@@ -29,7 +30,63 @@ void SsSetSpuInputAttr_Link(u8 source, u8 field, u8 value) asm("SsSetSpuInputAtt
 
 INCLUDE_RODATA("asm/PAL/main/nonmatchings/main/func_80043598", func_80043598_rodata);
 INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80043598", func_80043598);
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80043598", func_800437B8);
+
+void func_800437B8(void) {
+    s32 state;
+    s32 status;
+
+    state = D_8007F60C;
+    if (state == 1) {
+        goto state_1;
+    }
+    if (state >= 2) {
+        goto state_ge_2;
+    }
+    if (state == 0) {
+        goto state_0;
+    }
+    goto done;
+
+state_ge_2:
+    if (state == 2) {
+        goto state_2;
+    }
+    if (state == 3) {
+        goto state_3;
+    }
+    goto done;
+
+state_0:
+    if (func_8006A534(1, 0) == 0) {
+        goto done;
+    }
+    D_8007F60C = 1;
+state_1:
+    if (func_8006A5A4(3, 0, 0) == 0) {
+        goto done;
+    }
+    D_8007F60C = 2;
+    goto done;
+
+state_2:
+    status = func_8006A534(1, 0);
+    if (status == state) {
+        D_8007F60C = 3;
+        goto done;
+    }
+    if (status == 5) {
+        D_8007F60C = 1;
+        goto done;
+    }
+    goto done;
+
+state_3:
+    D_8007F604 = -1;
+    D_8007F60C = 0;
+
+done:
+    return;
+}
 
 void func_800438BC(void) {
     SsSetSpuInputAttr_Link(0, 0, 1);
