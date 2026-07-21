3a7c511c353cea3f601fcf5d849f141eca9b088c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-21 08:25:00 +0200

    Document func_80028DEC hand-asm (header + gte_* annotations)

--- src/main/PAL/main/func_80028DEC.c
+++ src/main/PAL/main/func_80028DEC.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+/*
+ * func_80028DEC: GTE geometry primitive (sibling of func_80028120 cluster). Hand-written asm: frameless, custom register-argument convention. Unmatchable as compiled C; documented inline asm.
+ */
 asm(
     ".set noreorder\n"
     ".set noat\n"
@@ -14,8 +17,8 @@ asm(
     ".word 0x8CE50000\n"
     ".word 0x10400017\n"
     ".word 0x20E80004\n"
-    ".word 0x484A0000\n"
-    ".word 0x484B0800\n"
+    ".word 0x484A0000\n"  /* cfc2 */
+    ".word 0x484B0800\n"  /* cfc2 */
     ".word 0x000A6400\n"
     ".word 0x000C6403\n"
     ".word 0x000C6022\n"
@@ -24,7 +27,7 @@ asm(
     ".word 0x000D6C00\n"
     ".word 0x318CFFFF\n"
     ".word 0x018D6025\n"
-    ".word 0x48CC0000\n"
+    ".word 0x48CC0000\n"  /* ctc2 */
     ".word 0x000B6400\n"
     ".word 0x000C6403\n"
     ".word 0x000C6022\n"
@@ -32,10 +35,10 @@ asm(
     ".word 0x000D6C00\n"
     ".word 0x318CFFFF\n"
     ".word 0x018D6025\n"
-    ".word 0x484A2800\n"
-    ".word 0x48CC0800\n"
+    ".word 0x484A2800\n"  /* cfc2 */
+    ".word 0x48CC0800\n"  /* ctc2 */
     ".word 0x000A5022\n"
-    ".word 0x48CA2800\n"
+    ".word 0x48CA2800\n"  /* ctc2 */
     ".word 0x10A00008\n"
     ".word 0xAC880080\n"
     ".word 0x0C00A3A7\n"
@@ -48,4 +51,5 @@ asm(
     ".word 0x8C9F008C\n"
     ".word 0x00000000\n"
     ".word 0x03E00008\n"
-    ".word 0x00000000\n");
+    ".word 0x00000000\n"
+);
