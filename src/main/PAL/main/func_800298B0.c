59e176c968cb319de4663c7b1d3401f12fa3b0bc
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-21 08:23:46 +0200

    Document 8 genuinely-hand-asm functions as annotated inline asm
    Converts INCLUDE_ASM stubs to documented inline-asm bodies for functions
    that were hand-written assembly in the original and cannot be compiled
    from C: 5 GTE primitives (frameless, custom register-ABI) with cop2 ops
    annotated by gte_* mnemonic, the MDEC decoder (pc-relative b branches),
    RotMatrix (32-bit multu), and a multi-entry cull loop. Each gets a header
    comment explaining what it does and why it stays asm. Byte-identical
    (make check passes).

--- src/main/PAL/main/func_800298B0.c
+++ src/main/PAL/main/func_800298B0.c
@@ -1,5 +1,371 @@
 #include "common.h"
 
-
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_800298B0", func_800298B0);
-
+/*
+ * func_800298B0: GTE depth-cue color + perspective transform (DPCS/INTPL/RTPS/RTPT/NCLIP), nested loops. Hand-written asm: frameless, custom register-argument convention. Unmatchable as compiled C.
+ */
+asm(
+    ".set noreorder\n"
+    ".set noat\n"
+    ".globl func_800298B0\n"
+    "func_800298B0:\n"
+    ".word 0xCB200000\n"  /* lwc2 */
+    ".word 0xCB210004\n"  /* lwc2 */
+    ".word 0x8DCC0000\n"
+    ".word 0x4A180001\n"  /* gte_rtps (RTPS) */
+    ".word 0x8DCD0004\n"
+    ".word 0xAC8C00A8\n"
+    ".word 0xAC8D00AC\n"
+    ".word 0x8DEC0000\n"
+    ".word 0x8DED0004\n"
+    ".word 0xAC8C00B0\n"
+    ".word 0xAC8D00B4\n"
+    ".word 0x8F0C0000\n"
+    ".word 0x8F0D0004\n"
+    ".word 0xAC8C00B8\n"
+    ".word 0xAC8D00BC\n"
+    ".word 0x8F2C0000\n"
+    ".word 0x8F2D0004\n"
+    ".word 0xAC8C00C0\n"
+    ".word 0xAC8D00C4\n"
+    ".word 0xC9060008\n"  /* lwc2 */
+    ".word 0x8D0C000C\n"
+    ".word 0x8C990084\n"
+    ".word 0x4A780010\n"  /* cop2 (GTE cmd) */
+    ".word 0x8D0D0010\n"
+    ".word 0x01996021\n"
+    ".word 0xAC8C00C8\n"
+    ".word 0xAC8D00CC\n"
+    ".word 0x950C0014\n"
+    ".word 0x950D0016\n"
+    ".word 0xA48C00D0\n"
+    ".word 0xA48D00D2\n"
+    ".word 0xE89600A0\n"  /* swc2 */
+    ".word 0x00C7C825\n"
+    ".word 0x1320012A\n"
+    ".word 0x4859A800\n"  /* cfc2 */
+    ".word 0x4858B000\n"  /* cfc2 */
+    ".word 0x484FB800\n"  /* cfc2 */
+    ".word 0xAC990110\n"
+    ".word 0xAC980114\n"
+    ".word 0xAC8F0118\n"
+    ".word 0x24191000\n"
+    ".word 0x00D93006\n"
+    ".word 0x00F93806\n"
+    ".word 0x8C9900A8\n"
+    ".word 0x8C9800AC\n"
+    ".word 0xAC9900DC\n"
+    ".word 0xAC9800E0\n"
+    ".word 0x8C9900B8\n"
+    ".word 0x8C9800BC\n"
+    ".word 0xAC9900EC\n"
+    ".word 0xAC9800F0\n"
+    ".word 0x949900C8\n"
+    ".word 0x949800D0\n"
+    ".word 0xA49900F6\n"
+    ".word 0xA49800FA\n"
+    ".word 0x24021000\n"
+    ".word 0x00461023\n"
+    ".word 0x48824000\n"  /* mtc2 */
+    ".word 0x8C9900DC\n"
+    ".word 0x8C9800E0\n"
+    ".word 0xAC9900D4\n"
+    ".word 0xAC9800D8\n"
+    ".word 0x8C9900EC\n"
+    ".word 0x8C9800F0\n"
+    ".word 0xAC9900E4\n"
+    ".word 0xAC9800E8\n"
+    ".word 0x909900C8\n"
+    ".word 0x909800C9\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x909900CC\n"
+    ".word 0x909800CD\n"
+    ".word 0x48994800\n"  /* mtc2 */
+    ".word 0x48985000\n"  /* mtc2 */
+    ".word 0x949900F6\n"
+    ".word 0x949800FA\n"
+    ".word 0xA49900F4\n"
+    ".word 0xA49800F8\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x909900D0\n"
+    ".word 0x909800D1\n"
+    ".word 0x908F00D2\n"
+    ".word 0x908E00D3\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x488F4800\n"  /* mtc2 */
+    ".word 0x488E5000\n"  /* mtc2 */
+    ".word 0xA09900F6\n"
+    ".word 0xA09800F7\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x849900A8\n"
+    ".word 0x849800AA\n"
+    ".word 0x848F00AC\n"
+    ".word 0x848E00B0\n"
+    ".word 0x848D00B2\n"
+    ".word 0x848C00B4\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48CFB800\n"  /* ctc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x488C5800\n"  /* mtc2 */
+    ".word 0xA09900FA\n"
+    ".word 0xA09800FB\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x849900B8\n"
+    ".word 0x849800BA\n"
+    ".word 0x848F00BC\n"
+    ".word 0x848E00C0\n"
+    ".word 0x848D00C2\n"
+    ".word 0x848C00C4\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48CFB800\n"  /* ctc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x480F5800\n"  /* mfc2 */
+    ".word 0x3339FFFF\n"
+    ".word 0x0018C400\n"
+    ".word 0x0338C825\n"
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x488C5800\n"  /* mtc2 */
+    ".word 0xAC9900DC\n"
+    ".word 0xAC8F00E0\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0xC88000D4\n"  /* lwc2 */
+    ".word 0xC88100D8\n"  /* lwc2 */
+    ".word 0xC88200DC\n"  /* lwc2 */
+    ".word 0xC88300E0\n"  /* lwc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x480F5800\n"  /* mfc2 */
+    ".word 0x3339FFFF\n"
+    ".word 0x4A280030\n"  /* gte_rtpt (RTPT) */
+    ".word 0x0018C400\n"
+    ".word 0x0338C825\n"
+    ".word 0xAC9900EC\n"
+    ".word 0xAC8F00F0\n"
+    ".word 0x949900F4\n"
+    ".word 0x949800F6\n"
+    ".word 0xA49900FC\n"
+    ".word 0xA49800FE\n"
+    ".word 0x24031000\n"
+    ".word 0xE88C0100\n"  /* swc2 */
+    ".word 0xE88D0104\n"  /* swc2 */
+    ".word 0x00671823\n"
+    ".word 0x48834000\n"  /* mtc2 */
+    ".word 0x849900D4\n"
+    ".word 0x849800D6\n"
+    ".word 0x848F00D8\n"
+    ".word 0x848E00E4\n"
+    ".word 0x848D00E6\n"
+    ".word 0x848C00E8\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48CFB800\n"  /* ctc2 */
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x488C5800\n"  /* mtc2 */
+    ".word 0x849900DC\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x849800DE\n"
+    ".word 0x848F00E0\n"
+    ".word 0x848E00EC\n"
+    ".word 0x848D00EE\n"
+    ".word 0x848C00F0\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48CFB800\n"  /* ctc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x480F5800\n"  /* mfc2 */
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x488C5800\n"  /* mtc2 */
+    ".word 0x3339FFFF\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x0018C400\n"
+    ".word 0x0338C825\n"
+    ".word 0x48990000\n"  /* mtc2 */
+    ".word 0x488F0800\n"  /* mtc2 */
+    ".word 0x909900F4\n"
+    ".word 0x909800F5\n"
+    ".word 0x908E00F8\n"
+    ".word 0x908D00F9\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x480F5800\n"  /* mfc2 */
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x3339FFFF\n"
+    ".word 0x0018C400\n"
+    ".word 0x0338C825\n"
+    ".word 0x48991000\n"  /* mtc2 */
+    ".word 0x488F1800\n"  /* mtc2 */
+    ".word 0x948F00CA\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x949900FC\n"
+    ".word 0x949800FE\n"
+    ".word 0x000F7C00\n"
+    ".word 0x032FC825\n"
+    ".word 0xACB9000C\n"
+    ".word 0xA4B8001C\n"
+    ".word 0x909900F6\n"
+    ".word 0x909800F7\n"
+    ".word 0x908E00FA\n"
+    ".word 0x908D00FB\n"
+    ".word 0x948F00CE\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x000F7C00\n"
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x488E4800\n"  /* mtc2 */
+    ".word 0x488D5000\n"  /* mtc2 */
+    ".word 0x0018C200\n"
+    ".word 0x4A980011\n"  /* cop2 (GTE cmd) */
+    ".word 0x333900FF\n"
+    ".word 0x0338C825\n"
+    ".word 0x032FC825\n"
+    ".word 0xA49900FC\n"
+    ".word 0xACB90014\n"
+    ".word 0x48194800\n"  /* mfc2 */
+    ".word 0x48185000\n"  /* mfc2 */
+    ".word 0x4A280030\n"  /* gte_rtpt (RTPT) */
+    ".word 0x333900FF\n"
+    ".word 0x0018C200\n"
+    ".word 0x0338C825\n"
+    ".word 0xA49900FE\n"
+    ".word 0xA4B90024\n"
+    ".word 0xE88C0108\n"  /* swc2 */
+    ".word 0xE88D010C\n"  /* swc2 */
+    ".word 0xC88E0104\n"  /* lwc2 */
+    ".word 0x8C990068\n"
+    ".word 0x00000000\n"
+    ".word 0x4B400006\n"  /* gte_nclip (NCLIP) */
+    ".word 0x4818C000\n"  /* mfc2 */
+    ".word 0x17200002\n"
+    ".word 0x00000000\n"
+    ".word 0x0018C022\n"
+    ".word 0x1F00000A\n"
+    ".word 0x00000000\n"
+    ".word 0xC88D0100\n"  /* lwc2 */
+    ".word 0x00000000\n"
+    ".word 0x00000000\n"
+    ".word 0x4B400006\n"  /* gte_nclip (NCLIP) */
+    ".word 0x4818C000\n"  /* mfc2 */
+    ".word 0x13200002\n"
+    ".word 0xC88D010C\n"  /* lwc2 */
+    ".word 0x0018C022\n"
+    ".word 0x1B00003D\n"
+    ".word 0x8499007C\n"
+    ".word 0x84980078\n"
+    ".word 0x848F0100\n"
+    ".word 0x848E0104\n"
+    ".word 0x848D0108\n"
+    ".word 0x848C010C\n"
+    ".word 0x032F082A\n"
+    ".word 0x10200006\n"
+    ".word 0x032E082A\n"
+    ".word 0x10200004\n"
+    ".word 0x032D082A\n"
+    ".word 0x10200002\n"
+    ".word 0x032C082A\n"
+    ".word 0x1420002F\n"
+    ".word 0x01F8082A\n"
+    ".word 0x10200006\n"
+    ".word 0x01D8082A\n"
+    ".word 0x10200004\n"
+    ".word 0x01B8082A\n"
+    ".word 0x10200002\n"
+    ".word 0x0198082A\n"
+    ".word 0x14200027\n"
+    ".word 0x8499007E\n"
+    ".word 0x8498007A\n"
+    ".word 0x848F0102\n"
+    ".word 0x848E0106\n"
+    ".word 0x848D010A\n"
+    ".word 0x848C010E\n"
+    ".word 0x032F082A\n"
+    ".word 0x10200006\n"
+    ".word 0x032E082A\n"
+    ".word 0x10200004\n"
+    ".word 0x032D082A\n"
+    ".word 0x10200002\n"
+    ".word 0x032C082A\n"
+    ".word 0x14200019\n"
+    ".word 0x01F8082A\n"
+    ".word 0x10200006\n"
+    ".word 0x01D8082A\n"
+    ".word 0x10200004\n"
+    ".word 0x01B8082A\n"
+    ".word 0x10200002\n"
+    ".word 0x0198082A\n"
+    ".word 0x14200011\n"
+    ".word 0x8C990100\n"
+    ".word 0x8C980104\n"
+    ".word 0x8C8F00A0\n"
+    ".word 0xACB90008\n"
+    ".word 0xACB80018\n"
+    ".word 0xACAF0004\n"
+    ".word 0x8C9900A4\n"
+    ".word 0x0005C200\n"
+    ".word 0x8F2F0000\n"
+    ".word 0x0018C202\n"
+    ".word 0x3C010900\n"
+    ".word 0x01E17825\n"
+    ".word 0xAF380000\n"
+    ".word 0xACAF0000\n"
+    ".word 0xE8AC0010\n"  /* swc2 */
+    ".word 0xE8AD0020\n"  /* swc2 */
+    ".word 0x20A50028\n"
+    ".word 0xE88C0100\n"  /* swc2 */
+    ".word 0x1460FF55\n"
+    ".word 0xE88D0104\n"  /* swc2 */
+    ".word 0x1440FEF5\n"
+    ".word 0x00000000\n"
+    ".word 0x8C990110\n"
+    ".word 0x8C980114\n"
+    ".word 0x8C8F0118\n"
+    ".word 0x48D9A800\n"  /* ctc2 */
+    ".word 0x48D8B000\n"  /* ctc2 */
+    ".word 0x48CFB800\n"  /* ctc2 */
+    ".word 0x03E00008\n"
+    ".word 0x00A01021\n"
+    ".word 0x8C990100\n"
+    ".word 0x8C980104\n"
+    ".word 0xACB90008\n"
+    ".word 0xACB80010\n"
+    ".word 0x8C990108\n"
+    ".word 0x8C98010C\n"
+    ".word 0xACB90018\n"
+    ".word 0xACB80020\n"
+    ".word 0x8C9900C8\n"
+    ".word 0x8C9800CC\n"
+    ".word 0xACB9000C\n"
+    ".word 0xACB80014\n"
+    ".word 0x949900D0\n"
+    ".word 0x949800D2\n"
+    ".word 0x8C8F00A0\n"
+    ".word 0xA4B9001C\n"
+    ".word 0xA4B80024\n"
+    ".word 0xACAF0004\n"
+    ".word 0x8C9900A4\n"
+    ".word 0x0005C200\n"
+    ".word 0x8F2F0000\n"
+    ".word 0x0018C202\n"
+    ".word 0x3C010900\n"
+    ".word 0x01E17825\n"
+    ".word 0xAF380000\n"
+    ".word 0xACAF0000\n"
+    ".word 0x03E00008\n"
+    ".word 0x20A20028\n"
+);
--- src/main/PAL/main/func_80069CC8.c
+++ src/main/PAL/main/func_80069CC8.c
@@ -31,4 +31,344 @@ Matrix *func_80069CC8(Matrix *src, Matrix *dst) {
     return ret;
 }
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80069CC8", func_80069D18);
+/*
+ * func_80069D18 = RotMatrix (psyq libgte): builds a 3x3 fixed-point rotation MATRIX from 3 Euler angles via the packed sin/cos table at 0x80094FAC. Hand-written asm: uses 14x 32-bit multu+mflo (GCC 2.6.3 never emits 32-bit multu, only mult) - unmatchable as compiled C, kept as documented asm.
+ */
+asm(
+    ".set noreorder
+"
+    ".set noat
+"
+    ".globl func_80069D18
+"
+    "func_80069D18:
+"
+    ".word 0x848F0000
+"
+    ".word 0x00A01021
+"
+    ".word 0x05E1000E
+"
+    ".word 0x31F90FFF
+"
+    ".word 0x000F7823
+"
+    ".word 0x05E10001
+"
+    ".word 0x31EF0FFF
+"
+    ".word 0x000FC080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x0019C400
+"
+    ".word 0x0018C403
+"
+    ".word 0x00185823
+"
+    ".word 0x0801A75F
+"
+    ".word 0x00194403
+"
+    ".word 0x0019C080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x0019C400
+"
+    ".word 0x00185C03
+"
+    ".word 0x00194403
+"
+    ".word 0x848F0002
+"
+    ".word 0x00000000
+"
+    ".word 0x05E1000E
+"
+    ".word 0x31F90FFF
+"
+    ".word 0x000F7823
+"
+    ".word 0x05E10001
+"
+    ".word 0x31EF0FFF
+"
+    ".word 0x000FC080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x00196400
+"
+    ".word 0x000C6403
+"
+    ".word 0x000C7023
+"
+    ".word 0x0801A779
+"
+    ".word 0x00194C03
+"
+    ".word 0x0019C080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x00197400
+"
+    ".word 0x000E7403
+"
+    ".word 0x000E6023
+"
+    ".word 0x00194C03
+"
+    ".word 0x012B0019
+"
+    ".word 0x848F0004
+"
+    ".word 0xA4AE0004
+"
+    ".word 0x0000C012
+"
+    ".word 0x0018C823
+"
+    ".word 0x00197303
+"
+    ".word 0x01280019
+"
+    ".word 0xA4AE000A
+"
+    ".word 0x05E10011
+"
+    ".word 0x31F90FFF
+"
+    ".word 0x0000C012
+"
+    ".word 0x00187303
+"
+    ".word 0xA4AE0010
+"
+    ".word 0x000F7823
+"
+    ".word 0x05E10001
+"
+    ".word 0x31EF0FFF
+"
+    ".word 0x000FC080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x0019C400
+"
+    ".word 0x0018C403
+"
+    ".word 0x00186823
+"
+    ".word 0x0801A79E
+"
+    ".word 0x00195403
+"
+    ".word 0x00007812
+"
+    ".word 0x000F7303
+"
+    ".word 0xA4AE0010
+"
+    ".word 0x0019C080
+"
+    ".word 0x3C198009
+"
+    ".word 0x0338C821
+"
+    ".word 0x8F394FAC
+"
+    ".word 0x00000000
+"
+    ".word 0x0019C400
+"
+    ".word 0x00186C03
+"
+    ".word 0x00195403
+"
+    ".word 0x01490019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000F7303
+"
+    ".word 0xA4AE0000
+"
+    ".word 0x01A90019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000F7023
+"
+    ".word 0x000E7B03
+"
+    ".word 0x014C0019
+"
+    ".word 0xA4AF0002
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000FC303
+"
+    ".word 0x00000000
+"
+    ".word 0x030B0019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000F7303
+"
+    ".word 0x00000000
+"
+    ".word 0x01A80019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000FCB03
+"
+    ".word 0x032E7823
+"
+    ".word 0x03080019
+"
+    ".word 0xA4AF0006
+"
+    ".word 0x00000000
+"
+    ".word 0x00007012
+"
+    ".word 0x000E7B03
+"
+    ".word 0x00000000
+"
+    ".word 0x01AB0019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007012
+"
+    ".word 0x000ECB03
+"
+    ".word 0x032F7021
+"
+    ".word 0x01AC0019
+"
+    ".word 0xA4AE000C
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000FC303
+"
+    ".word 0x00000000
+"
+    ".word 0x030B0019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000F7303
+"
+    ".word 0x00000000
+"
+    ".word 0x01480019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007812
+"
+    ".word 0x000FCB03
+"
+    ".word 0x032E7821
+"
+    ".word 0x03080019
+"
+    ".word 0xA4AF0008
+"
+    ".word 0x00000000
+"
+    ".word 0x00007012
+"
+    ".word 0x000E7B03
+"
+    ".word 0x00000000
+"
+    ".word 0x014B0019
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+    ".word 0x00007012
+"
+    ".word 0x000ECB03
+"
+    ".word 0x032F7023
+"
+    ".word 0xA4AE000E
+"
+    ".word 0x03E00008
+"
+    ".word 0x00000000
+"
+    ".word 0x00000000
+"
+);
