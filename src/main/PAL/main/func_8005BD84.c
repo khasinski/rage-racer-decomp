db7b705816bafaea5ab7bc00a1568fb9a1434fe7
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-22 23:41:22 +0200

    Decompile func_8005BD84

--- src/main/PAL/main/func_8005BD84.c
+++ src/main/PAL/main/func_8005BD84.c
@@ -1,3 +1,28 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/PAL/main", func_8005BD84);
+void func_80043134(s32 arg0);
+void func_8005E834(s32 arg0);
+
+void func_8005BD84(s32 arg0) {
+    register s32 value asm("$16");
+
+    value = arg0;
+    asm(".globl func_8005BDB0\nfunc_8005BDB0 = func_8005BD84 + 0x2C" : "=r"(value) : "0"(value));
+    if (value < 0) {
+        goto negative;
+    }
+    if (arg0 < 0x10) {
+        goto call;
+    }
+
+    arg0 = 0xF;
+    goto call;
+
+negative:
+    arg0 = 0;
+
+call:
+    value = arg0;
+    func_80043134(arg0);
+    func_8005E834(value);
+}
