7f7dc12e0b7f6e0d2c09d5b62ce50e083bd8acd5
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 11:09:21 +0200

    Remove register constraints from func_80050F68

--- src/main/PAL/main/func_80050F68.c
+++ src/main/PAL/main/func_80050F68.c
@@ -4,14 +4,13 @@
 extern GameCarEntry *D_8019C7C8;
 
 s32 func_80050F68(void) {
-    register s32 count asm("$5") = 0;
-    register s32 i asm("$4") = 0;
-    register GameCarEntry *ptr asm("$3") = D_8019C7C8;
+    s32 count = 0;
+    s32 i = 0;
+    GameCarEntry *entries = D_8019C7C8;
     s32 flag;
 
     do {
-        flag = ptr->enabled;
-        ptr++;
+        flag = entries[i].enabled;
         if (flag != 0) {
             count++;
         }
