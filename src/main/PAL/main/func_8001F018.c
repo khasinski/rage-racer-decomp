3a97ca25605a114befd6384b95f7a900911e1e9f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 10:45:06 +0200

    Remove register constraints from func_8001F018

--- src/main/PAL/main/func_8001F018.c
+++ src/main/PAL/main/func_8001F018.c
@@ -6,39 +6,40 @@ s32 func_8006DD30(s32 arg0);
 s32 func_8006CD0C(s32 arg0);
 
 void func_8001F018(void *arg0) {
-    register s32 status asm("$3");
-    register s32 two asm("$2");
     u8 byte;
 
-    do {
-        while (func_8006A534(1, 0) == 0) {
-        }
+outer:
+    while (func_8006A534(1, 0) == 0) {
+    }
 
-        byte = 0x80;
-        while (func_8006A5A4(0xE, &byte, 0) == 0) {
-        }
+    byte = 0x80;
+    while (func_8006A5A4(0xE, &byte, 0) == 0) {
+    }
 
-        {
-            do {
-                status = func_8006A534(1, 0);
-                two = 2;
-            } while (status == 0);
-
-            if (status == two) {
-                func_8006DD30(3);
-            }
+pollFirst:
+    switch (func_8006A534(1, 0)) {
+    case 0:
+        goto pollFirst;
+    case 2:
+        func_8006DD30(3);
+        break;
+    }
 
 send:
-            while (func_8006A5A4(0x15, arg0, 0) == 0) {
-            }
+    while (func_8006A5A4(0x15, arg0, 0) == 0) {
+    }
 
-            do {
-                status = func_8006A534(1, 0);
-                two = 2;
-            } while (status == 0);
-            if (status != two) {
-                goto send;
-            }
-        }
-    } while (func_8006CD0C(0x1E0) == 0);
+pollNext:
+    switch (func_8006A534(1, 0)) {
+    case 0:
+        goto pollNext;
+    case 2:
+        break;
+    default:
+        goto send;
+    }
+
+    if (func_8006CD0C(0x1E0) == 0) {
+        goto outer;
+    }
 }
