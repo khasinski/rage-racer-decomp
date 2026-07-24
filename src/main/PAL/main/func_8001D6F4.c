08b2d45fc64c42408dcfd22fa2867248762275b4
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 13:24:51 +0200

    Remove asm from func_8001D6F4

--- src/main/PAL/main/func_8001D6F4.c
+++ src/main/PAL/main/func_8001D6F4.c
@@ -7,23 +7,25 @@ extern s16 D_8019CB3C;
 void func_8001D6F4(u32 arg0, u32 arg1) {
     u32 a;
     u32 b;
-    register u32 c asm("$2");
-    register u32 high asm("$3");
-    register s32 bias asm("$6");
+    u32 c;
+    u32 high;
+    s32 bias;
 
     a = arg0 >> 1;
     b = arg1 >> 1;
     a &= 0x3DEF;
     b &= 0x3DEF;
     c = a + b;
+    high = c;
     bias = 0x8000;
-    __asm__ volatile("addu %0,%1,%2" : "=r"(high) : "r"(c), "r"(bias));
+    high += bias;
     c >>= 1;
     c &= 0x3DEF;
     a += c;
-    a -= bias;
+    /* Preserve the bias while keeping its two GCC live ranges separate. */
+    a -= bias++;
     c += b;
-    c -= bias;
+    c -= --bias;
 
     D_8019CB3A = high;
     D_8019CB38 = a;
