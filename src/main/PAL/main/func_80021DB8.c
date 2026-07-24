28de130b49e8470bed6022bb2c2d9c324204da63
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 14:30:50 +0200

    Decompile func_80021DB8

--- src/main/PAL/main/func_80021DB8.c
+++ src/main/PAL/main/func_80021DB8.c
@@ -1,7 +1,5 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/PAL/main/nonmatchings/main/func_80021DB8", func_80021DB8);
-
 typedef struct {
     s8 pad[8];
     s32 v8;
@@ -9,20 +7,105 @@ typedef struct {
     s16 vE;
 } S22;
 
-extern char D_80010FA8[], D_80010F98[], D_80010F9C[], D_80010FA4[];
+extern char D_80010F90[], D_80010FA8[], D_80010F98[], D_80010F9C[], D_80010FA4[];
+extern s32 D_8009E858[];
 extern s32 D_801E4BA8;
 extern s16 D_8019CABC;
 extern s32 D_801E428C;
+extern s32 D_8019CAC8;
 extern s32 D_8019CE10;
+extern s32 D_801E8A48;
 extern u8 *D_8007D4C0[];
 extern s32 D_8007D508[];
 extern s32 D_8007D4D4[];
+extern S22 D_801E7744[][4][5];
 extern S22 D_8019CB78[][4][5];
 
 void func_80016EA0(void *dst, s32 len, void *src, s32 arg3);
 void func_80016754(void *dst, s32 x, void *src, s32 color);
 void *func_80021CD4(void *dst, s32 value);
 void func_800632F0();
+
+void func_80021DB8(u8 *arg0) {
+    register u8 *panel;
+    register s32 iter;
+    register s32 countOrIndex;
+    register s32 xOrField;
+    register s32 destination;
+    register s32 color;
+    register s32 scoreOrX;
+    char text[56];
+    s32 mode;
+    s32 row;
+    s32 doubledRow;
+    s32 value;
+    s32 scoreValue;
+    s32 limit;
+
+    panel = arg0;
+    func_80016EA0(panel + 0x10, 0x4C, D_80010F90, 0x7852);
+    mode = D_801E428C;
+    text[1] = 0x2F;
+    limit = 6;
+    if (mode != 3) {
+        limit = 3;
+    }
+    iter = 0;
+    if (limit > 0) {
+        scoreOrX = (s32)D_8009E858;
+        do {
+            scoreValue = iter + (((u32)iter) >> 31);
+            row = scoreValue >> 1;
+            doubledRow = row * 2;
+            value = iter - doubledRow;
+            value <<= 3;
+            xOrField = value + 0x58;
+            func_80021CD4(
+                &text[2],
+                ({
+                    (void)(*(volatile char *)&text[0] = iter + 0x31);
+                    doubledRow = (doubledRow + row) << 5;
+                    scoreValue = *((s32 *)scoreOrX);
+                    value = (destination = (s32)(panel + 0x14));
+                    scoreValue;
+                }));
+            destination = doubledRow;
+            destination = destination + value;
+            color = 0x78CC;
+            if (D_8019CAC8 == iter) {
+                color = 0x780F;
+            }
+            func_80016754((void *)destination, xOrField, text, color);
+            iter++;
+            scoreOrX += 4;
+        } while (iter < limit);
+    }
+    func_80016EA0(panel + 0x10, 0x6C, D_80010F98, 0x7812);
+    countOrIndex = 0;
+    scoreOrX = 0x82;
+    destination = 0x78;
+    do {
+        text[0] = D_8007D4C0[countOrIndex][0];
+        text[1] = D_8007D4C0[countOrIndex][1];
+        text[2] = D_8007D4C0[countOrIndex][2];
+        text[3] = 0x2F;
+        func_80021CD4(&text[4], D_801E7744[D_8019CABC][D_801E428C][countOrIndex].v8);
+        xOrField = D_801E7744[D_8019CABC][D_801E428C][countOrIndex].vC;
+        func_800632F0(&text[0xC], D_80010F9C,
+                      &D_801E7744[D_8019CABC][D_801E428C][countOrIndex],
+                      D_8007D508[xOrField]);
+        color = 0x78CC;
+        if (D_801E8A48 == countOrIndex) {
+            color = 0x780F;
+        }
+        func_80016754(panel + 0x14, destination, text, color);
+        func_800632F0(text, D_80010FA4, D_8007D4D4[xOrField]);
+        func_80016754(panel + 0x2C, scoreOrX, text, color);
+        destination += 0x14;
+        scoreOrX += 0x14;
+        countOrIndex++;
+    } while (countOrIndex < 5);
+}
 
 void func_80022068(u8 *s5) {
     char text[48];
