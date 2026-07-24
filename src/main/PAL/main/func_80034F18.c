98e7ed18d7c25a5600ea06c20d34aaa7146b330b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 11:08:23 +0200

    Remove register constraints from func_80034F18

--- src/main/PAL/main/func_80034F18.c
+++ src/main/PAL/main/func_80034F18.c
@@ -1,14 +1,12 @@
 #include "common.h"
 
 s32 func_80034F18(s32 arg0, s32 arg1) {
-    register s32 quotient asm("$3");
-    s32 remainder;
-    register s32 remainderPart asm("$4");
+    s32 quotient;
     s32 quotientPart;
 
     quotient = arg0 / 25;
-    remainder = arg0 - (quotient * 25);
-    remainderPart = (remainder * 40) + arg1;
+    arg0 -= quotient * 25;
+    arg0 = (arg0 * 40) + arg1;
     quotientPart = quotient * 1000;
-    return quotientPart + remainderPart;
+    return quotientPart + arg0;
 }
