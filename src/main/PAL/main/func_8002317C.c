c693c61df81e93e357df102ac0a301e7947e6eda
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 11:59:06 +0200

    Remove register constraints from func_8002317C

--- src/main/PAL/main/func_8002317C.c
+++ src/main/PAL/main/func_8002317C.c
@@ -7,12 +7,12 @@ void *func_80016F8C(void *arg0, void *arg1, s32 arg2, s32 arg3, s32 arg4, s32 ar
 void *func_80017390(void *arg0, void *arg1, s32 arg2);
 
 void func_8002317C(void) {
-    register u8 *base asm("$16");
-    register s32 height asm("$17");
-    register s32 clut asm("$18");
-    register void **scratch asm("$19");
-    register s32 fade asm("$20");
-    register s32 value asm("$3");
+    u8 *base;
+    s32 height;
+    s32 clut;
+    void **scratch;
+    s32 fade;
+    s32 value;
     void *next;
 
     fade = D_801E40B8;
@@ -29,7 +29,8 @@ void func_8002317C(void) {
     base = D_8019C900 + 0xCC;
     scratch = (void **)0x1F800000;
 
-    next = func_80016F8C(base, *scratch, 0x64, 0xEC, 0x7C, 0x18, 0x80, 0, 0x3F97, fade);
+    next = *scratch;
+    next = func_80016F8C(base, next, 0x64, 0xEC, 0x7C, 0x18, 0x80, 0, 0x3F97, fade);
 
     height = 0x20;
     clut = 0x3FD7;
