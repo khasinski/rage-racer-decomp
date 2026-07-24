22c7e0990629942c9d8cb14bbc4b174ec07d1bb8
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 11:58:04 +0200

    Remove register constraints from func_8001A3C0

--- src/main/PAL/main/func_8001A3C0.c
+++ src/main/PAL/main/func_8001A3C0.c
@@ -3,23 +3,25 @@
 void func_8001A2E0(void *arg0);
 
 void func_8001A3C0(void *arg0) {
-    register u8 *ptr asm("$4");
-    register s32 offset asm("$16");
-    register u8 *next asm("$16");
+    union {
+        s32 offset;
+        u8 *next;
+    } state;
+    u8 *ptr;
 
     ptr = (u8 *)arg0 + 4;
     goto test;
 
 loop:
-    offset = (u32)offset >> 2;
-    offset <<= 2;
-    next = ptr + offset;
+    state.offset = (u32)state.offset >> 2;
+    state.offset <<= 2;
+    state.next = ptr + state.offset;
     func_8001A2E0(ptr);
-    ptr = next;
+    ptr = state.next;
 
 test:
-    offset = *(s32 *)ptr;
-    if (offset > 0) {
+    state.offset = *(s32 *)ptr;
+    if (state.offset > 0) {
         ptr += 4;
         goto loop;
     }
