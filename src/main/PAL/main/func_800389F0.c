a8423be88c922e32f1b14aadab597d818477cf95
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 12:05:35 +0200

    Remove register constraints from func_800389F0

--- src/main/PAL/main/func_800389F0.c
+++ src/main/PAL/main/func_800389F0.c
@@ -7,11 +7,11 @@ void func_80017A10(s32 arg0);
 void func_8001DFC0(GameCarRuntime *arg0);
 
 void func_800389F0(void) {
-    register GameCarRuntime *base asm("s1");
-    register s32 i asm("s2");
-    register u8 *entry asm("s0");
-    register s32 one asm("s3");
-    register s32 minus_one asm("s4");
+    GameCarRuntime *base;
+    s32 i;
+    u8 *entry;
+    s32 one;
+    s32 minus_one;
 
     base = D_801F1854;
     func_80017A10(1);
