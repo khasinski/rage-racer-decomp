7432a080fcb570b2c6d8cb579e8cb50d756214ff
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 11:10:12 +0200

    Decompile func_80021288 in C

--- src/main/PAL/main/func_80021288.c
+++ src/main/PAL/main/func_80021288.c
@@ -1,42 +1,28 @@
 #include "common.h"
 
 typedef struct {
-    u32 unk0;
-    u32 unk4;
+    u8 raw[8];
 } UnkCopyChunk;
 
-extern u8 D_8007BE68;
+extern UnkCopyChunk D_8007BE68[];
 
 void func_80021288(void *arg0, s32 *arg1) {
-    register s32 *out asm("t1") = arg1;
+    UnkCopyChunk *dst;
+    UnkCopyChunk *src;
+    s32 i;
 
-    asm volatile("" : "=r"(out) : "0"(out));
-    asm volatile(
-        "move  $t0,$zero\n"
-        "move  $a3,%0\n"
-        "lui   $a2,%%hi(D_8007BE68)\n"
-        "addiu $a2,$a2,%%lo(D_8007BE68)\n"
-        "1:\n"
-        "lwl   $v0,3($a2)\n"
-        "lwr   $v0,0($a2)\n"
-        "lwl   $v1,7($a2)\n"
-        "lwr   $v1,4($a2)\n"
-        "swl   $v0,3($a3)\n"
-        "swr   $v0,0($a3)\n"
-        "swl   $v1,7($a3)\n"
-        "swr   $v1,4($a3)\n"
-        "addiu $a3,$a3,8\n"
-        "addiu $t0,$t0,1\n"
-        "slti  $v0,$t0,13\n"
-        "bnez  $v0,1b\n"
-        "addiu $a2,$a2,8\n"
-        :
-        : "r"(arg0)
-        : "$2", "$3", "$6", "$7", "$8", "memory");
+    i = 0;
+    dst = arg0;
+    src = D_8007BE68;
+    do {
+        *dst = *src;
+        dst++;
+        i++;
+    } while (src++, i < 13);
 
-    out[1] = 3;
-    out[0] = 0;
-    out[2] = 0;
-    out[3] = -1;
-    out[4] = 0;
+    arg1[1] = 3;
+    arg1[0] = 0;
+    arg1[2] = 0;
+    arg1[3] = -1;
+    arg1[4] = 0;
 }
