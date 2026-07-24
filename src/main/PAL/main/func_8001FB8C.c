90ece74c6ea5d6f99e073950f57a2f1d9a6750ea
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-24 09:49:37 +0200

    Remove register constraints from func_8001FB8C

--- src/main/PAL/main/func_8001FB8C.c
+++ src/main/PAL/main/func_8001FB8C.c
@@ -8,14 +8,16 @@ s32 func_80016EC4(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s3
 s32 func_80017390(s32 arg0, s32 arg1, s32 arg2);
 
 void func_8001FB8C(void) {
-    register volatile s32 *scratch asm("$17");
-    register s32 base asm("$16");
+    volatile s32 *scratch;
+    s32 base;
     s32 next;
+    s32 value;
 
     if ((D_801E40B8 & 0x10) && (D_8019C8EC == 0)) {
         scratch = (volatile s32 *)0x1F800000;
+        value = *scratch;
         base = D_8019C900 + 0xCC;
-        next = func_80016EC4(base, *scratch, 0x10, 0x10, 0x48, 0x10, 0, 0x68, 0x780D);
+        next = func_80016EC4(base, value, 0x10, 0x10, 0x48, 0x10, 0, 0x68, 0x780D);
         *scratch = func_80017390(base, next, 9);
     }
 }
