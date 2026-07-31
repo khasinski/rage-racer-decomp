1d26e2b05db6e833750e77874819eaabb108f148
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-07-31 23:36:46 +0200

    Collapse GameAtan2 decompiler temps into returns

--- src/main/PAL/main/render/GameBuildRotMatrixZ.c
+++ src/main/PAL/main/render/GameBuildRotMatrixZ.c
@@ -88,9 +88,6 @@ void GameSetCameraRotMatrix(void) {
 extern s16 g_AtanTable[] asm("D_8007B664");
 
 s32 GameAtan2(s32 arg0, s32 arg1) {
-    s32 temp_v0;
-    s32 temp_v1;
-
     if (arg0 == 0) {
         if (arg1 == 0) {
             return 0;
@@ -104,8 +101,7 @@ s32 GameAtan2(s32 arg0, s32 arg1) {
     if (arg0 > 0) {
         if (arg1 >= 0) {
             if (arg0 < arg1) {
-                temp_v1 = g_AtanTable[(arg0 << 10) / arg1];
-                return 0x400 - temp_v1;
+                return 0x400 - g_AtanTable[(arg0 << 10) / arg1];
             }
             return g_AtanTable[(arg1 << 10) / arg0];
         }
@@ -122,15 +118,12 @@ s32 GameAtan2(s32 arg0, s32 arg1) {
         if (arg0 < arg1) {
             return g_AtanTable[(arg0 << 10) / arg1] + 0x400;
         }
-        temp_v1 = g_AtanTable[(arg1 << 10) / arg0];
-        return 0x800 - temp_v1;
+        return 0x800 - g_AtanTable[(arg1 << 10) / arg0];
     }
 
     arg1 = -arg1;
     if (arg0 < arg1) {
-        temp_v1 = g_AtanTable[(arg0 << 10) / arg1];
-        temp_v0 = 0xC00;
-        return temp_v0 - temp_v1;
+        return 0xC00 - g_AtanTable[(arg0 << 10) / arg1];
     }
     return g_AtanTable[(arg1 << 10) / arg0] + 0x800;
 }
