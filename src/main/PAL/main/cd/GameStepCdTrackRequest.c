399e6624e19984e438ae0cdb95cab562002850cb
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-07-31 23:50:55 +0200

    Rewrite CD track/play request step dispatch as structured switch

--- src/main/PAL/main/cd/GameStepCdTrackRequest.c
+++ src/main/PAL/main/cd/GameStepCdTrackRequest.c
@@ -23,56 +23,56 @@ void GameStepCdTrackRequest(void) {
     switch (g_CdTrackStep) {
     case 0:
         if (func_8006A534(1, 0) == 0) {
-            goto done;
+            break;
         }
         g_CdFadeFrames = 0;
         g_CdTrackStep = 1;
     case 1:
         if (CdControl(0x16, &g_CdTrackLocs[g_CdTrackPending], 0) == 0) {
-            goto done;
+            break;
         }
         g_CdTrackStep = 2;
-        goto done;
+        break;
     case 2:
         state = func_8006A534(1, 0);
         if (state == 2) {
             g_CdTrackStep = 3;
-            goto done;
+            break;
         }
         if (state == 5) {
             g_CdTrackStep = 1;
-            goto done;
+            break;
         }
-        goto done;
+        break;
     case 3:
         status = g_CdVolume;
         track = (g_CdCurrentTrack = *(u8 *)&g_CdTrackPending);
         g_CdTrackPending = -1;
         g_CdTrackStep = 0;
         GameSetCdVolume(status);
-        goto done;
+        break;
     case 4:
         if (func_8006A534(1, 0) == 0) {
-            goto done;
+            break;
         }
         g_CdTrackStep = 5;
     case 5:
         if (CdControl(0x16, &g_CdTrackLocs[g_CdTrackPending], 0) == 0) {
-            goto done;
+            break;
         }
         g_CdTrackStep = 6;
-        goto done;
+        break;
     case 6:
         state = func_8006A534(1, 0);
         if (state == 2) {
             g_CdTrackStep = 7;
-            goto done;
+            break;
         }
         if (state == 5) {
             g_CdTrackStep = state;
-            goto done;
+            break;
         }
-        goto done;
+        break;
     case 7:
         track = *(u8 *)&g_CdTrackPending;
         g_CdTrackPending = -1;
@@ -80,9 +80,6 @@ void GameStepCdTrackRequest(void) {
         g_CdCurrentTrack = track;
         break;
     }
-
-done:
-    return;
 }
 
 void GameStepCdPlayRequest(void) asm("func_80043494");
@@ -91,54 +88,32 @@ void GameStepCdPlayRequest(void) {
     s32 status;
 
     state = g_CdCommandStep;
-    if (state == 1) {
-        goto state_1;
-    }
-    if (state >= 2) {
-        goto state_ge_2;
-    }
-    if (state == 0) {
-        goto state_0;
-    }
-    goto done;
-
-state_ge_2:
-    if (state == 2) {
-        goto state_2;
-    }
-    if (state == 3) {
-        goto state_3;
-    }
-    goto done;
-
-state_0:
-    if (func_8006A534(1, 0) == 0) {
-        goto done;
-    }
-    g_CdCommandStep = 1;
-state_1:
-    if (CdControl(3, 0, 0) == 0) {
-        goto done;
-    }
-    g_CdCommandStep = 2;
-    goto done;
-
-state_2:
-    status = func_8006A534(1, 0);
-    if (status == state) {
-        g_CdCommandStep = 3;
-        goto done;
-    }
-    if (status == 5) {
+    switch (state) {
+    case 0:
+        if (func_8006A534(1, 0) == 0) {
+            break;
+        }
         g_CdCommandStep = 1;
-        goto done;
+    case 1:
+        if (CdControl(3, 0, 0) == 0) {
+            break;
+        }
+        g_CdCommandStep = 2;
+        break;
+    case 2:
+        status = func_8006A534(1, 0);
+        if (status == state) {
+            g_CdCommandStep = 3;
+            break;
+        }
+        if (status == 5) {
+            g_CdCommandStep = 1;
+            break;
+        }
+        break;
+    case 3:
+        g_CdCommandPending = -1;
+        g_CdCommandStep = 0;
+        break;
     }
-    goto done;
-
-state_3:
-    g_CdCommandPending = -1;
-    g_CdCommandStep = 0;
-
-done:
-    return;
 }
