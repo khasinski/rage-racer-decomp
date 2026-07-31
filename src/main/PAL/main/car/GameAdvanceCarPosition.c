ab5f79b5296bc5fbf5266e2af895727046ff9452
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-07-31 22:45:01 +0200

    Rewrite GameAdvanceCarPosition with arg0 directly (drops 1 pin)
    (cherry picked from commit 79191ccfd7a1f61a29dafdaa7f90dc089578f4e5)

--- src/main/PAL/main/car/GameAdvanceCarPosition.c
+++ src/main/PAL/main/car/GameAdvanceCarPosition.c
@@ -14,26 +14,24 @@ s32 func_80068634(s32 arg0);
  */
 void GameAdvanceCarPosition(GameCarRuntime *arg0) asm("func_8002F4E4");
 void GameAdvanceCarPosition(GameCarRuntime *arg0) {
-    /* This pin is load-bearing: removing it changes .text. */
-    register GameCarRuntime *car asm("$19") = arg0;
     volatile s32 coords[3];
 
     {
         s32 angleSin;
         s32 otherSin;
 
-        angleSin = func_80068568(car->headingAngle);
-        otherSin = func_80068568(car->field_24);
-        coords[0] = (((angleSin * car->field_A4) / 4) + (otherSin * car->field_A8)) / 100;
+        angleSin = func_80068568(arg0->headingAngle);
+        otherSin = func_80068568(arg0->field_24);
+        coords[0] = (((angleSin * arg0->field_A4) / 4) + (otherSin * arg0->field_A8)) / 100;
     }
 
     {
         s32 angleCos;
         s32 otherCos;
 
-        angleCos = func_80068634(car->headingAngle);
-        otherCos = func_80068634(car->field_24);
-        coords[2] = (((angleCos * car->field_A4) / 4) + (otherCos * car->field_A8)) / 100;
+        angleCos = func_80068634(arg0->headingAngle);
+        otherCos = func_80068634(arg0->field_24);
+        coords[2] = (((angleCos * arg0->field_A4) / 4) + (otherCos * arg0->field_A8)) / 100;
     }
 
     {
@@ -42,12 +40,12 @@ void GameAdvanceCarPosition(GameCarRuntime *arg0) {
         s32 angleCos;
         s32 otherCos;
 
-        angleSin = func_80068568(car->headingAngle);
-        otherSin = func_80068568(car->field_24);
-        angleCos = func_80068634(car->headingAngle);
-        otherCos = func_80068634(car->field_24);
+        angleSin = func_80068568(arg0->headingAngle);
+        otherSin = func_80068568(arg0->field_24);
+        angleCos = func_80068634(arg0->headingAngle);
+        otherCos = func_80068634(arg0->field_24);
 
-        car->field_A4 += ((((angleSin * otherSin) + (angleCos * otherCos)) / 4096) * car->field_A8) / 4096;
+        arg0->field_A4 += ((((angleSin * otherSin) + (angleCos * otherCos)) / 4096) * arg0->field_A8) / 4096;
     }
-    car->headingAngle = 0x400 - GameAtan2(coords[0], coords[2]);
+    arg0->headingAngle = 0x400 - GameAtan2(coords[0], coords[2]);
 }
