47b08805fb2e4399ce71863bf67d615de4793bc1
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-02 17:41:40 +0200

    Scope low_mode body in Gpu_LoadTexImageAndGetTPage so its gotos become break

--- src/main/PAL/main/sdk/Gpu_LoadTexImageAndGetTPage.c
+++ src/main/PAL/main/sdk/Gpu_LoadTexImageAndGetTPage.c
@@ -140,18 +140,19 @@ long GetTPage(long arg0, long arg1, long arg2, long arg3) {
     long value;
 
     mode = func_800657E4();
+    switch (0) { default:
     if (!(mode == 1)) {
 
     mode = func_800657E4();
     if (mode != 2) {
-        goto low_mode;
+        break;
     }
     }
 
     value = ((arg0 & 3) << 9) | ((arg1 & 3) << 7) | ((arg3 & 0x300) >> 3);
     return value | ((arg2 & 0x3FF) >> 6);
 
-low_mode:
+    }
     value = ((arg0 & 3) << 7) | ((arg1 & 3) << 5) | ((arg3 & 0x100) >> 4) | ((arg2 & 0x3FF) >> 6);
     return value | ((arg3 & 0x200) << 2);
 }
