2d55b71dc7fcbc8f01f4a799e9bde2c47ad28fd5
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-08-09 05:27:06 +0200

    Use rectangles for team name uploads

--- src/main/PAL/main/menu/team_name_texture.c
+++ src/main/PAL/main/menu/team_name_texture.c
@@ -2,24 +2,24 @@
 #include "game/menu.h"
 
 void ClearTeamNameTexture(void) {
-    s16 sp[4];
-    sp[0] = 0x282;
-    sp[1] = 0x37;
-    sp[2] = 0xC;
-    sp[3] = 8;
-    LoadImage((Rect *)sp, &g_TeamNameBlankTile);
+    Rect rect;
+    rect.x = 0x282;
+    rect.y = 0x37;
+    rect.w = 0xC;
+    rect.h = 8;
+    LoadImage(&rect, &g_TeamNameBlankTile);
 }
 
 void UploadTeamNameTexture(u8 *str, s32 len) {
-    s16 sp[4];
+    Rect rect;
     ClearTeamNameTexture();
-    sp[0] = 0x288 - len;
-    sp[1] = 0x37;
-    sp[2] = 2;
-    sp[3] = 8;
+    rect.x = 0x288 - len;
+    rect.y = 0x37;
+    rect.w = 2;
+    rect.h = 8;
     while (len > 0) {
-        LoadImage((Rect *)sp, &g_TeamNameFontGlyphs[*str++ << 5]);
-        sp[0] += 2;
+        LoadImage(&rect, &g_TeamNameFontGlyphs[*str++ << 5]);
+        rect.x += 2;
         len--;
     }
 }
