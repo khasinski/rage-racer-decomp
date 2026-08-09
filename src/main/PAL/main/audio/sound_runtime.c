cb29da51728f788addd48ef25fa0c5992c41f8a5
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-08-09 02:13:30 +0200

    Use typed music channel reset

--- src/main/PAL/main/audio/sound_runtime.c
+++ src/main/PAL/main/audio/sound_runtime.c
@@ -56,20 +56,17 @@ void ResetSoundState(void) {
     {
         s32 neg;
         s32 *ptr;
-        s32 offset;
 
         i = 0;
         neg = -1;
         ptr = &g_AudioSlotMask;
-        offset = 0;
         for (; i < 2; i++) {
-            *(s32 *)((u8 *)&g_MusicChannels[0].mode + offset) = neg;
-            *(s32 *)((u8 *)&g_MusicChannels[0].left + offset) = neg;
-            *(s32 *)((u8 *)&g_MusicChannels[0].right + offset) = neg;
+            g_MusicChannels[i].mode = neg;
+            g_MusicChannels[i].left.value = neg;
+            g_MusicChannels[i].right.value = neg;
             ptr[0x78 / 4] = 0;
             ptr = (s32 *)((u8 *)ptr + 0x18);
-            *(s32 *)((u8 *)&g_MusicChannels[0].volLeft + offset) = 0;
-            offset += 0x18;
+            g_MusicChannels[i].volLeft = 0;
         }
     }
 
