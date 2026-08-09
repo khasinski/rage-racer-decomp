4d74ea67c3d8641340e89421a1eacca439817f61
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-08-09 04:33:41 +0200

    Initialize the FMV context through fields

--- src/main/PAL/main/fmv/fmv_decode.c
+++ src/main/PAL/main/fmv/fmv_decode.c
@@ -49,43 +49,39 @@ void EndFmv(void) {
     g_StreamReturnScene = g_FmvStreamEnded;
 }
 
-void InitFmvContext(FmvDecodeContext *ctx, s32 width, s32 height) {
-    volatile u32 *words;
-    volatile u16 *halves;
-    u32 word0;
-    u32 word1;
-    u32 word3;
-    u32 word4;
+void InitFmvContext(volatile FmvDecodeContext *ctx, s32 width, s32 height) {
+    volatile u32 *word0;
+    volatile u32 *word1;
+    volatile u32 *word3;
+    volatile u32 *word4;
     u16 half18;
     u16 half1A;
     u16 half20;
     u32 word28;
     u16 half22;
 
-    words = ctx;
-    halves = ctx;
-    word0 = (u32)g_FmvVlcBuffer0;
-    word1 = (u32)g_FmvVlcBuffer1;
-    word3 = (u32)g_FmvStripBuffer0;
-    word4 = (u32)g_FmvStripBuffer1;
-    words[2] = 0;
-    words[5] = 0;
-    words[0] = word0;
-    words[1] = word1;
-    words[3] = word3;
-    words[4] = word4;
+    word0 = g_FmvVlcBuffer0;
+    word1 = g_FmvVlcBuffer1;
+    word3 = g_FmvStripBuffer0;
+    word4 = g_FmvStripBuffer1;
+    ctx->vlcIndex = 0;
+    ctx->stripIndex = 0;
+    ctx->vlcBuffers[0] = word0;
+    ctx->vlcBuffers[1] = word1;
+    ctx->stripBuffers[0] = word3;
+    ctx->stripBuffers[1] = word4;
     half18 = g_DispEnv0X;
-    halves[0xC] = half18;
+    ctx->displayRects[0].x = half18;
     half1A = g_DispEnv0Y;
-    halves[0xD] = half1A;
+    ctx->displayRects[0].y = half1A;
     half20 = g_DispEnv1X;
-    halves[0x10] = half20;
+    ctx->displayRects[1].x = half20;
     word28 = g_FrameParity;
     half22 = g_DispEnv1Y;
-    halves[0x16] = width;
-    halves[0x17] = height;
-    words[0xD] = 0;
-    words[0xA] = word28;
-    halves[0x18] = 0x18;
-    ((u16 *)ctx)[0x11] = half22;
+    ctx->stripWidth = width;
+    ctx->stripHeight = height;
+    ctx->decodeComplete = 0;
+    ctx->frameParity = word28;
+    ctx->sliceHeight = 0x18;
+    ((FmvDecodeContext *)ctx)->displayRects[1].y = half22;
 }
