c530a1dbfa64d951cb4736c026cc97bc8d7f4ba8
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-08-10 06:16:29 +0200

    Type sprite string render packets

--- src/main/PAL/main/render/sprite_string.c
+++ src/main/PAL/main/render/sprite_string.c
@@ -4,6 +4,7 @@
 #include "game/prim.h"
 #include "game/render.h"
 #include "game/render_internal.h"
+#include "game/render_types.h"
 #include "game/scratchpad.h"
 #include "psyq/gpu.h"
 
@@ -20,19 +21,22 @@ void DrawSpriteString(long x, long y, u_char *str, long clutIndex) {
     volatile SPRT *oldPacket;
     u_char *otv;
     u_char *tableB;
+    RenderBufferAddress packetAddress;
 
     sr = str;
     next = SCRATCH_PRIM_CURSOR_AS(u_char);
     if (*sr != 0) {
         tableA = g_SpriteFontU;
         tableB = g_SpriteFontV;
-        packet = (volatile SPRT *)next;
+        packetAddress.bytes = next;
+        packet = packetAddress.volatileSprite;
         do {
             idx = *sr++ - 0x20;
             if (idx != 0) {
                 ga = tableA[idx * 2];
                 gb = tableB[idx * 2];
-                SetSprt((SPRT *)next);
+                packetAddress.bytes = next;
+                SetSprt(packetAddress.sprite);
                 SetShadeTex(next, 1);
                 next += 0x14;
                 oldPacket = packet;
@@ -46,12 +50,14 @@ void DrawSpriteString(long x, long y, u_char *str, long clutIndex) {
                 packet->clut = clutIndex;
                 packet->w = w;
                 packet++;
-                AddPrim(otv + 0xCC, (SPRT *)oldPacket);
+                packetAddress.volatileSprite = oldPacket;
+                AddPrim(otv + 0xCC, packetAddress.sprite);
             }
             x += g_SpriteFontWidth[idx];
         } while (*sr != 0);
     }
-    SetDrawMode((DrawPacket *)next, 0, 1, 0x1D, g_DrawModeEnv);
+    packetAddress.bytes = next;
+    SetDrawMode(packetAddress.drawPacket, 0, 1, 0x1D, g_DrawModeEnv);
     AddPrim(g_DrawBuffer + 0xCC, next);
     SCRATCH_PRIM_CURSOR_AS(u_char) = next + 0xC;
 }
