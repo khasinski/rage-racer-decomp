20df7dec12abdf8e7f1e094f2574c77a616b4cf5
Chris Hasinski <krzysztof.hasinski@gmail.com>
2026-09-03 10:28:30 +0200

    Decompile Japanese driving update

---
+++ src/main/JAP10/main/car/update_car_driving.c
@@ -0,0 +1,2 @@
+#define CAR_DRIVING_IGNORE_SHIFT_STATE
+#include "../../../PAL/main/car/update_car_driving.c"
--- src/main/PAL/main/car/update_car_driving.c
+++ src/main/PAL/main/car/update_car_driving.c
@@ -51,8 +51,11 @@ void UpdateCarDriving(PlayerCarRuntime *car, s32 unused) {
     spec = g_CarSpec;
     if (spec->redline + 1000 < route->engineRpm) {
         s16 v = g_SteerHoldFrames;
-        if (v >= 41 && route->gear == spec->topGear &&
-            car->shiftState == 0) {
+        if (v >= 41 && route->gear == spec->topGear
+#ifndef CAR_DRIVING_IGNORE_SHIFT_STATE
+            && car->shiftState == 0
+#endif
+        ) {
             idx = v + 24;
             if (idx >= 101) {
                 idx = 100;
